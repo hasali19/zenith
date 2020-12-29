4e4677c9d3038e86c2e28bda1a1bc893ad896a6d
Hasan Ali <git@hasali.co.uk>
2020-12-29 17:17:45 +0000

    Add server selection screen

--- zenith_android/app/src/main/java/uk/co/hasali/zenith/MainActivity.kt
+++ zenith_android/app/src/main/java/uk/co/hasali/zenith/MainActivity.kt
@@ -1,11 +1,10 @@
 package uk.co.hasali.zenith
 
+import android.app.Application
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
@@ -18,13 +17,18 @@ import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.res.loadVectorResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
+import androidx.datastore.preferences.core.preferencesKey
+import androidx.lifecycle.AndroidViewModel
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 import uk.co.hasali.zenith.ui.ZenithTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ZenithApp()
+            ZenithApp(settingsRepo = UserSettingsRepository.getInstance(this))
         }
     }
 }
@@ -39,21 +43,44 @@ sealed class Screen(val name: String, val icon: @Composable () -> Unit) {
     object TvShows : Screen("TV Shows", {
         loadVectorResource(id = R.drawable.television).resource.resource?.let { Icon(it) }
     })
+
+    object SelectServer : Screen("Select Server", {})
 }
 
 @Composable
-fun ZenithApp() {
+fun ZenithApp(settingsRepo: UserSettingsRepository) {
+    val scope = rememberCoroutineScope()
+
+    val settings by settingsRepo.settings.collectAsState(initial = null)
+    if (settings == null) {
+        return
+    }
+
+    val serverUrl = settings!!.serverUrl
     var currentScreen: Screen by remember { mutableStateOf(Screen.Home) }
+    val screen = if (serverUrl == null) Screen.SelectServer else currentScreen
+
     ZenithTheme {
-        TopLevelScreenScaffold(
-            screens = listOf(Screen.Home, Screen.Movies, Screen.TvShows),
-            currentScreen = currentScreen,
-            onScreenChange = { currentScreen = it }
-        ) {
-            when (currentScreen) {
-                is Screen.Home -> HomeScreen()
-                is Screen.Movies -> MoviesScreen()
-                is Screen.TvShows -> TvShowsScreen()
+        when (screen) {
+            is Screen.SelectServer -> SelectServerScreen(onSave = {
+                scope.launch {
+                    settingsRepo.setServerUrl(it)
+                }
+            })
+
+            else -> {
+                TopLevelScreenScaffold(
+                    screens = listOf(Screen.Home, Screen.Movies, Screen.TvShows),
+                    currentScreen = currentScreen,
+                    onScreenChange = { currentScreen = it }
+                ) {
+                    when (currentScreen) {
+                        is Screen.Home -> HomeScreen()
+                        is Screen.Movies -> MoviesScreen()
+                        is Screen.TvShows -> TvShowsScreen()
+                        else -> throw IllegalStateException()
+                    }
+                }
             }
         }
     }
@@ -117,5 +144,36 @@ fun TvShowsScreen() {
             modifier = Modifier.align(Alignment.Center).size(48.dp),
             colorFilter = ColorFilter.tint(Color.DarkGray)
         )
+    }
+}
+
+@Composable
+fun SelectServerScreen(onSave: (String) -> Unit) {
+    var url by remember { mutableStateOf("") }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text(text = "Select Server") })
+        }
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            TextField(
+                value = url,
+                onValueChange = { url = it },
+                label = { Text("Server address") },
+                modifier = Modifier.fillMaxWidth(),
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = { onSave(url) },
+                modifier = Modifier.align(Alignment.End)
+            ) {
+                Text(text = "Save")
+            }
+        }
     }
 }
