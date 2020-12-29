30e6a945ff5c3b949cf9736bb2a68fda136f5bbf
Hasan Ali <git@hasali.co.uk>
2020-12-29 15:12:00 +0000

    Scaffold top level app screens

--- zenith_android/app/src/main/java/uk/co/hasali/zenith/MainActivity.kt
+++ zenith_android/app/src/main/java/uk/co/hasali/zenith/MainActivity.kt
@@ -2,37 +2,120 @@ package uk.co.hasali.zenith
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.Text
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.setContent
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.loadVectorResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
 import uk.co.hasali.zenith.ui.ZenithTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ZenithTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            ZenithApp()
+        }
+    }
+}
+
+sealed class Screen(val name: String, val icon: @Composable () -> Unit) {
+    object Home : Screen("Home", { Icon(Icons.Filled.Home) })
+
+    object Movies : Screen("Movies", {
+        loadVectorResource(id = R.drawable.movie).resource.resource?.let { Icon(it) }
+    })
+
+    object TvShows : Screen("TV Shows", {
+        loadVectorResource(id = R.drawable.television).resource.resource?.let { Icon(it) }
+    })
+}
+
+@Composable
+fun ZenithApp() {
+    var currentScreen: Screen by remember { mutableStateOf(Screen.Home) }
+    ZenithTheme {
+        TopLevelScreenScaffold(
+            screens = listOf(Screen.Home, Screen.Movies, Screen.TvShows),
+            currentScreen = currentScreen,
+            onScreenChange = { currentScreen = it }
+        ) {
+            when (currentScreen) {
+                is Screen.Home -> HomeScreen()
+                is Screen.Movies -> MoviesScreen()
+                is Screen.TvShows -> TvShowsScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TopLevelScreenScaffold(
+    screens: List<Screen>,
+    currentScreen: Screen,
+    onScreenChange: (Screen) -> Unit,
+    content: @Composable () -> Unit
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text(text = "Zenith") })
+        },
+        bottomBar = {
+            BottomNavigation {
+                screens.forEach { screen ->
+                    BottomNavigationItem(
+                        icon = screen.icon,
+                        label = { Text(screen.name) },
+                        selected = currentScreen.name == screen.name,
+                        onClick = { if (currentScreen.name != screen.name) onScreenChange(screen) }
+                    )
+                }
+            }
+        }
+    ) {
+        content()
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    ZenithTheme {
-        Greeting("Android")
+fun HomeScreen() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            imageVector = Icons.Default.Home,
+            modifier = Modifier.align(Alignment.Center).size(48.dp),
+            colorFilter = ColorFilter.tint(Color.DarkGray)
+        )
+    }
+}
+
+@Composable
+fun MoviesScreen() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            imageVector = vectorResource(id = R.drawable.movie),
+            modifier = Modifier.align(Alignment.Center).size(48.dp),
+            colorFilter = ColorFilter.tint(Color.DarkGray)
+        )
+    }
+}
+
+@Composable
+fun TvShowsScreen() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            imageVector = vectorResource(id = R.drawable.television),
+            modifier = Modifier.align(Alignment.Center).size(48.dp),
+            colorFilter = ColorFilter.tint(Color.DarkGray)
+        )
     }
 }
