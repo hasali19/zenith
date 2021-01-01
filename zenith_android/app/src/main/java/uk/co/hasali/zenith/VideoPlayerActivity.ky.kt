19ba04f5b5f13b70bd605d742435fd6a87d56cbf
Hasan Ali <git@hasali.co.uk>
2021-01-01 15:53:07 +0000

    Use fullscreen mode for video player

--- zenith_android/app/src/main/java/uk/co/hasali/zenith/VideoPlayerActivity.ky.kt
+++ zenith_android/app/src/main/java/uk/co/hasali/zenith/VideoPlayerActivity.ky.kt
@@ -1,6 +1,8 @@
 package uk.co.hasali.zenith
 
+import android.os.Build
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.google.android.exoplayer2.MediaItem
@@ -38,8 +40,39 @@ class VideoPlayerActivity : AppCompatActivity() {
         }
     }
 
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            hideSystemUi()
+        }
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         player?.release()
+    }
+
+    private fun hideSystemUi() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {
+            @Suppress("DEPRECATION")
+            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN)
+        } else {
+            TODO()
+        }
+    }
+
+    private fun showSystemUi() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {
+            @Suppress("DEPRECATION")
+            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+        } else {
+            TODO()
+        }
     }
 }
