ec2fff95b5daadb3469d85f6b0c4a7e5165a8902
Hasan Ali <git@hasali.co.uk>
2021-01-02 10:23:52 +0000

    Implement basic custom player controls

--- zenith_android/app/src/main/java/uk/co/hasali/zenith/VideoPlayerActivity.ky.kt
+++ zenith_android/app/src/main/java/uk/co/hasali/zenith/VideoPlayerActivity.ky.kt
@@ -3,27 +3,37 @@ package uk.co.hasali.zenith
 import android.os.Build
 import android.os.Bundle
 import android.support.v4.media.session.MediaSessionCompat
-import android.view.SurfaceView
-import android.view.View
-import android.view.ViewGroup
-import android.widget.FrameLayout
+import android.support.v4.media.session.PlaybackStateCompat
+import android.view.*
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.setContent
-import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.lifecycleScope
+import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.fuel.coroutines.awaitObject
+import com.github.kittinunf.fuel.gson.gsonDeserializer
 import com.google.android.exoplayer2.*
 import com.google.android.exoplayer2.ext.mediasession.MediaSessionConnector
+import com.google.android.exoplayer2.ui.AspectRatioFrameLayout
 import com.google.android.exoplayer2.video.VideoListener
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 
 class VideoPlayerActivity : AppCompatActivity() {
+
+    data class StreamInfo(val duration: Float)
+
+    enum class PlaybackState {
+        PLAYING,
+        PAUSED,
+    }
 
     private var player: SimpleExoPlayer? = null
     private var session: MediaSessionCompat? = null
@@ -31,21 +41,27 @@ class VideoPlayerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_video_player)
 
         val streamId = intent.getIntExtra("stream_id", -1)
 
-        var aspectRatio by mutableStateOf(16f / 9f)
-        val surfaceView = SurfaceView(this).apply {
-            layoutParams = ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.MATCH_PARENT,
-            )
-        }
+        val aspectRatioLayout: AspectRatioFrameLayout = findViewById(R.id.aspect_ratio_layout)
+        val surfaceView: SurfaceView = findViewById(R.id.surface_view)
+        val composeView: ComposeView = findViewById(R.id.compose_view)
+
+        var playbackState by mutableStateOf(PlaybackState.PLAYING)
+        var playbackPosition by mutableStateOf(0L)
+        var duration by mutableStateOf(0L)
 
         lifecycleScope.launch {
             val settingsRepo = UserSettingsRepository.getInstance(this@VideoPlayerActivity)
             val settings = settingsRepo.settings.first()
             val serverUrl = settings.serverUrl!!
+
+            val info: StreamInfo = Fuel.get("$serverUrl/api/stream/$streamId/info")
+                .awaitObject(gsonDeserializer())
+
+            duration = info.duration.toLong()
 
             player = SimpleExoPlayer.Builder(this@VideoPlayerActivity)
                 .build()
@@ -60,9 +76,21 @@ class VideoPlayerActivity : AppCompatActivity() {
                             pixelWidthHeightRatio: Float
                         ) {
                             // Set the aspect ratio for the SurfaceView
-                            aspectRatio =
+                            val aspectRatio =
                                 if (width == 0 || height == 0) 1f
                                 else (width * pixelWidthHeightRatio) / height
+
+                            aspectRatioLayout.setAspectRatio(aspectRatio)
+                        }
+                    })
+
+                    addListener(object : Player.EventListener {
+                        override fun onPlayWhenReadyChanged(playWhenReady: Boolean, reason: Int) {
+                            if (playWhenReady) {
+                                playbackState = PlaybackState.PLAYING
+                            } else {
+                                playbackState = PlaybackState.PAUSED
+                            }
                         }
                     })
 
@@ -71,6 +99,13 @@ class VideoPlayerActivity : AppCompatActivity() {
                     prepare()
                     play()
                 }
+
+            launch {
+                while (player.let { it != null && it.playbackState != Player.STATE_ENDED }) {
+                    playbackPosition = player?.currentPosition ?: 0
+                    delay(1000)
+                }
+            }
 
             val session = MediaSessionCompat(this@VideoPlayerActivity, "ZenithMediaSession").apply {
                 isActive = true
@@ -83,12 +118,31 @@ class VideoPlayerActivity : AppCompatActivity() {
             }
         }
 
-        setContent {
-            Box(modifier = Modifier.fillMaxSize().background(Color.Black)) {
-                AndroidView(
-                    viewBlock = { FrameLayout(it).apply { addView(surfaceView) } },
-                    modifier = Modifier.aspectRatio(aspectRatio).align(Alignment.Center),
-                )
+        composeView.setContent {
+            val position = playbackPosition.toFloat() / 1000
+            val range = 0f..duration.toFloat()
+
+            Box(modifier = Modifier.fillMaxSize()) {
+                Column(modifier = Modifier.align(Alignment.BottomCenter).padding(16.dp)) {
+                    Slider(position, {}, valueRange = range)
+                    Row(
+                        horizontalArrangement = Arrangement.Center,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        FloatingActionButton(onClick = {
+                            player?.let { it.playWhenReady = !it.playWhenReady }
+                        }) {
+                            Icon(
+                                vectorResource(
+                                    id = when (playbackState) {
+                                        PlaybackState.PAUSED -> R.drawable.play
+                                        PlaybackState.PLAYING -> R.drawable.pause
+                                    }
+                                )
+                            )
+                        }
+                    }
+                }
             }
         }
     }
