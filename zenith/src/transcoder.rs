8055b33496c2d761668d6645a9afa350cfe18476
Hasan Ali <git@hasali.co.uk>
2021-02-20 17:43:55 +0000

    Cache hls playlists on filesystem

--- zenith/src/transcoder.rs
+++ zenith/src/transcoder.rs
@@ -1,11 +1,13 @@
 use std::collections::HashMap;
+use std::path::Path;
 use std::process::Stdio;
 use std::sync::Arc;
 use std::time::Duration;
 
 use bytes::Bytes;
 use futures::StreamExt;
-use tokio::io::{AsyncBufReadExt, BufReader};
+use itertools::Itertools;
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader, BufWriter};
 use tokio::sync::{oneshot, Mutex};
 use zenith_http::Body;
 
@@ -13,15 +15,65 @@ use crate::config::Config;
 use crate::db::Db;
 use crate::ffmpeg::{Ffmpeg, HlsTranscodeOptions};
 
+struct Playlist(Vec<(f64, f64)>);
+
+pub struct PlaylistCache;
+
+impl PlaylistCache {
+    async fn read_playlist(&self, item_id: i64) -> eyre::Result<Option<Playlist>> {
+        let file = match tokio::fs::File::open(format!("cache/playlists/{}", item_id)).await {
+            Ok(file) => file,
+            Err(e) => match e.kind() {
+                std::io::ErrorKind::NotFound => return Ok(None),
+                _ => return Err(eyre::eyre!(e)),
+            },
+        };
+
+        let reader = BufReader::new(file);
+        let mut lines = reader.lines();
+        let mut playlist = vec![];
+
+        while let Some(line) = lines.next_line().await? {
+            let (start_time, duration) = line
+                .split(' ')
+                .tuples()
+                .next()
+                .ok_or_else(|| eyre::eyre!(""))?;
+
+            playlist.push((start_time.parse().unwrap(), duration.parse().unwrap()));
+        }
+
+        Ok(Some(Playlist(playlist)))
+    }
+
+    async fn write_playlist(&self, item_id: i64, playlist: &Playlist) -> eyre::Result<()> {
+        if !Path::new("cache/playlists").is_dir() {
+            tokio::fs::create_dir_all("cache/playlists").await?;
+        }
+
+        let file = tokio::fs::File::create(format!("cache/playlists/{}", item_id)).await?;
+        let mut writer = BufWriter::new(file);
+
+        for (start_time, duration) in &playlist.0 {
+            writer
+                .write(format!("{} {}\n", start_time, duration).as_bytes())
+                .await?;
+        }
+
+        Ok(())
+    }
+}
+
 pub struct HlsTranscoder {
     db: Db,
     config: Arc<Config>,
+    playlist_cache: Mutex<PlaylistCache>,
     current: Mutex<Option<JobState>>,
 }
 
 struct JobState {
     item_id: i64,
-    playlist: Vec<(f64, f64)>,
+    playlist: Playlist,
     segments: HashMap<u32, Bytes>,
     last_requested_segment: u32,
     canceller: Option<oneshot::Sender<()>>,
@@ -32,6 +84,7 @@ impl HlsTranscoder {
         HlsTranscoder {
             db,
             config,
+            playlist_cache: Mutex::new(PlaylistCache),
             current: Mutex::new(None),
         }
     }
@@ -45,52 +98,69 @@ impl HlsTranscoder {
             }
         }
 
-        let mut conn = self.db.acquire().await?;
+        let playlist_cache = self.playlist_cache.lock().await;
 
-        let (path, duration): (String, f64) =
-            sqlx::query_as("SELECT path, duration FROM video_files WHERE item_id = ?")
-                .bind(item_id)
-                .fetch_optional(&mut conn)
-                .await?
-                .ok_or_else(|| eyre::eyre!("not found"))?;
-
-        let mut child = tokio::process::Command::new("ffprobe")
-            .arg("-select_streams")
-            .arg("v")
-            .arg("-skip_frame")
-            .arg("nokey")
-            .arg("-show_entries")
-            .arg("frame=pkt_pts_time")
-            .arg("-print_format")
-            .arg("csv")
-            .arg(path)
-            .stdout(Stdio::piped())
-            .spawn()
-            .unwrap();
-
-        let mut playlist = vec![];
-
-        let stdout = child.stdout.as_mut().unwrap();
-        let reader = BufReader::new(stdout);
-        let mut lines = reader.lines();
-
-        let line = lines.next_line().await.unwrap().unwrap();
-
-        let mut start_time: f64 = line.split(',').nth(1).unwrap().parse().unwrap();
-
-        while let Ok(Some(v)) = lines.next_line().await {
-            let frame_time: f64 = v.split(',').nth(1).unwrap().parse().unwrap();
-            let duration = frame_time - start_time;
-            if duration >= 6.0 {
-                playlist.push((start_time, duration));
-                start_time = frame_time;
+        let playlist = match playlist_cache.read_playlist(item_id).await? {
+            Some(playlist) => {
+                log::info!("using cached playlist");
+                playlist
             }
-        }
+            None => {
+                log::info!("generating new playlist");
 
-        let final_segment = duration - start_time;
-        if final_segment > 0.0 {
-            playlist.push((start_time, final_segment));
-        }
+                let mut conn = self.db.acquire().await?;
+
+                let (path, duration): (String, f64) =
+                    sqlx::query_as("SELECT path, duration FROM video_files WHERE item_id = ?")
+                        .bind(item_id)
+                        .fetch_optional(&mut conn)
+                        .await?
+                        .ok_or_else(|| eyre::eyre!("not found"))?;
+
+                let mut child = tokio::process::Command::new("ffprobe")
+                    .arg("-select_streams")
+                    .arg("v")
+                    .arg("-skip_frame")
+                    .arg("nokey")
+                    .arg("-show_entries")
+                    .arg("frame=pkt_pts_time")
+                    .arg("-print_format")
+                    .arg("csv")
+                    .arg(path)
+                    .stdout(Stdio::piped())
+                    .stderr(Stdio::null())
+                    .spawn()
+                    .unwrap();
+
+                let mut playlist = vec![];
+
+                let stdout = child.stdout.as_mut().unwrap();
+                let reader = BufReader::new(stdout);
+                let mut lines = reader.lines();
+
+                let line = lines.next_line().await.unwrap().unwrap();
+
+                let mut start_time: f64 = line.split(',').nth(1).unwrap().parse().unwrap();
+
+                while let Ok(Some(v)) = lines.next_line().await {
+                    let frame_time: f64 = v.split(',').nth(1).unwrap().parse().unwrap();
+                    let duration = frame_time - start_time;
+                    if duration >= 6.0 {
+                        playlist.push((start_time, duration));
+                        start_time = frame_time;
+                    }
+                }
+
+                let final_segment = duration - start_time;
+                if final_segment > 0.0 {
+                    playlist.push((start_time, final_segment));
+                }
+
+                let playlist = Playlist(playlist);
+                playlist_cache.write_playlist(item_id, &playlist).await?;
+                playlist
+            }
+        };
 
         if let Some(JobState {
             canceller: Some(canceller),
@@ -127,7 +197,7 @@ impl HlsTranscoder {
         playlist.push_str("#EXT-X-TARGETDURATION:6\n");
         playlist.push_str("#EXT-X-MEDIA-SEQUENCE:0\n");
 
-        for (i, (_, length)) in job.playlist.iter().enumerate() {
+        for (i, (_, length)) in job.playlist.0.iter().enumerate() {
             playlist.push_str(&format!("#EXTINF:{:.8},\n", length));
             playlist.push_str(&format!("{}.ts\n", i));
         }
@@ -201,7 +271,7 @@ impl HlsTranscoder {
 
         let mut child = ffmpeg.transcode_hls(&HlsTranscodeOptions {
             input_path: &path,
-            start_time: job.playlist[segment as usize].0,
+            start_time: job.playlist.0[segment as usize].0,
             start_number: segment,
             segment_time: 6,
         })?;
