3344538c21db86319ad7b04eb45d08159458f18b
Hasan Ali <git@hasali.co.uk>
2021-03-14 17:44:48 +0000

    Set theme color in app manifest

--- zenith_web/vue.config.js
+++ zenith_web/vue.config.js
@@ -3,6 +3,7 @@ module.exports = {
     manifestOptions: {
       name: 'Zenith',
       short_name: 'Zenith', // eslint-disable-line
+      theme_color: '#1f1f1f', // eslint-disable-line
     },
   },
   transpileDependencies: ['vuetify'],
