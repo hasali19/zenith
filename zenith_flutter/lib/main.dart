ef0c7101b6c3ccc4e819aa3d5616469932259702
Hasan Ali <hasan@hasali.dev>
2021-04-17 23:12:07 +0100

    Implement basic update downloader

--- zenith_flutter/lib/main.dart
+++ zenith_flutter/lib/main.dart
@@ -1,113 +1,85 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(App());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class App extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return AppState();
+  }
+}
+
+const GIT_COMMIT_HASH = bool.hasEnvironment('GIT_COMMIT_HASH')
+    ? String.fromEnvironment('GIT_COMMIT_HASH')
+    : null;
+
+Future fetchRuns() async {
+  var res = await http.get(
+    Uri.https(
+      'api.github.com',
+      'repos/hasali19/zenith/actions/workflows/8229171/runs',
+      {'per_page': "1"},
+    ),
+  );
+
+  final runs = jsonDecode(res.body);
+  final run = runs['workflow_runs'][0];
+  final runId = run['id'];
+  final hash = run['head_sha'];
+  final suiteId = run['check_suite_id'];
+
+  if (hash != GIT_COMMIT_HASH) {
+    res = await http.get(
+      Uri.https(
+        'api.github.com',
+        'repos/hasali19/zenith/actions/runs/$runId/artifacts',
+      ),
+    );
+
+    final artifacts = jsonDecode(res.body);
+    final artifactId = artifacts['artifacts'][0]['id'];
+    final url =
+        'https://github.com/hasali19/zenith/suites/$suiteId/artifacts/$artifactId';
+
+    await canLaunch(url) ? await launch(url) : throw 'Could not launch $url';
+  }
+}
+
+class AppState extends State<App> {
+  Future _runId;
+
+  @override
+  void initState() {
+    super.initState();
+    _runId = fetchRuns();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      title: 'Zenith',
+      home: Scaffold(
+        body: Center(
+          child: FutureBuilder(
+            future: _runId,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Text("${snapshot.data}");
+              } else if (snapshot.hasError) {
+                return Text("${snapshot.error}");
+              } else {
+                return CircularProgressIndicator();
+              }
+            },
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
