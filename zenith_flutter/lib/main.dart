57c442581058475f16c33b841ca8f2b1e404d594
Hasan Ali <hasan@hasali.dev>
2021-04-18 14:43:50 +0100

    Add movie details screen

--- zenith_flutter/lib/main.dart
+++ zenith_flutter/lib/main.dart
@@ -61,7 +61,7 @@ Future<List<Movie>> _fetchMovies() async {
 }
 
 class AppState extends State<App> {
-  Future _movies;
+  Future<List<Movie>> _movies;
 
   @override
   void initState() {
@@ -76,72 +76,157 @@ class AppState extends State<App> {
       themeMode: ThemeMode.system,
       theme: ThemeData.light(),
       darkTheme: ThemeData.dark(),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Zenith"),
-        ),
-        body: Center(
-          child: FutureBuilder<List<Movie>>(
-            future: _movies,
-            builder: (context, snapshot) {
-              if (snapshot.hasError) {
-                return Text('${snapshot.error}');
-              }
+      home: MovieListScreen(_movies),
+    );
+  }
+}
 
-              if (!snapshot.hasData) {
-                return CircularProgressIndicator();
-              }
+class MovieListScreen extends StatelessWidget {
+  final Future<List<Movie>> _movies;
 
-              return GridView.extent(
-                maxCrossAxisExtent: 150,
-                childAspectRatio: 2 / 3.8,
-                padding: EdgeInsets.all(8),
-                crossAxisSpacing: 8,
-                children: snapshot.data
-                    .map(
-                      (movie) => Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          Material(
-                            elevation: 2.0,
-                            type: MaterialType.card,
-                            clipBehavior: Clip.hardEdge,
-                            child: Ink.image(
-                              fit: BoxFit.cover,
-                              image: NetworkImage(movie.poster),
-                              child: InkWell(
-                                onTap: () {},
-                                child: AspectRatio(aspectRatio: 2 / 3),
-                              ),
-                            ),
-                          ),
-                          Container(
-                            padding: EdgeInsets.symmetric(vertical: 8),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.stretch,
-                              children: [
-                                TextOneLine(
-                                  movie.title,
-                                  style: Theme.of(context).textTheme.subtitle2,
-                                  overflow: TextOverflow.fade,
-                                ),
-                                Text(
-                                  movie.releaseYear().toString(),
-                                  maxLines: 1,
-                                  style: Theme.of(context).textTheme.caption,
-                                )
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
-                    )
-                    .toList(),
-              );
-            },
+  MovieListScreen(this._movies);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Movies"),
+      ),
+      body: FutureBuilder<List<Movie>>(
+        future: _movies,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Text('${snapshot.error}');
+          }
+
+          if (!snapshot.hasData) {
+            return CircularProgressIndicator();
+          }
+
+          return GridView.extent(
+            maxCrossAxisExtent: 150,
+            childAspectRatio: 2 / 3.8,
+            padding: EdgeInsets.all(8),
+            crossAxisSpacing: 8,
+            children: [for (final movie in snapshot.data) PosterCard(movie)],
+          );
+        },
+      ),
+    );
+  }
+}
+
+String formatDuration(double duration) {
+  if (duration <= 90 * 60) {
+    return '${(duration / 60).floor()}m';
+  } else {
+    final hours = (duration / 3600).floor();
+    final minutes = ((duration % 3600) / 60).floor();
+    return '${hours}h ${minutes}m';
+  }
+}
+
+class MovieDetailsScreen extends StatelessWidget {
+  final Movie movie;
+
+  MovieDetailsScreen(this.movie);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Scaffold(
+      appBar: AppBar(),
+      body: ListView(
+        // crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          AspectRatio(
+            aspectRatio: 16 / 9,
+            child: Image.network(movie.backdrop),
+          ),
+          Container(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  movie.title,
+                  style: theme.textTheme.headline4,
+                ),
+                Text(
+                  '${movie.releaseYear()} • ${formatDuration(movie.duration)}',
+                  style: theme.textTheme.caption,
+                ),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  padding: EdgeInsets.symmetric(vertical: 4),
+                  child: ElevatedButton.icon(
+                    onPressed: () {},
+                    icon: Icon(Icons.play_arrow, size: 18),
+                    label: Text("Play"),
+                  ),
+                ),
+                Text(
+                  movie.overview,
+                  style: theme.textTheme.bodyText2,
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class PosterCard extends StatelessWidget {
+  final Movie movie;
+
+  PosterCard(this.movie);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Material(
+          elevation: 2.0,
+          type: MaterialType.card,
+          clipBehavior: Clip.hardEdge,
+          child: Ink.image(
+            fit: BoxFit.cover,
+            image: NetworkImage(movie.poster),
+            child: InkWell(
+              child: AspectRatio(aspectRatio: 2 / 3),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => MovieDetailsScreen(movie),
+                  ),
+                );
+              },
+            ),
           ),
         ),
-      ),
+        Container(
+          padding: EdgeInsets.symmetric(vertical: 8),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextOneLine(
+                movie.title,
+                style: Theme.of(context).textTheme.subtitle2,
+                overflow: TextOverflow.fade,
+              ),
+              Text(
+                movie.releaseYear().toString(),
+                maxLines: 1,
+                style: Theme.of(context).textTheme.caption,
+              )
+            ],
+          ),
+        )
+      ],
     );
   }
 }
