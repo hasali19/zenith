ef0c5bc7aa40104f831445b61199ece64696d23b
Hasan Ali <hasan@hasali.dev>
2021-05-02 10:47:32 +0100

    Create android app project

---
+++ client/android/app/src/main/java/uk/hasali/zenith/MainActivity.kt
@@ -0,0 +1,11 @@
+package uk.hasali.zenith
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
