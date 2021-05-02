e492f80f5c51021d2ab0d26907b8bafa055701fa
Hasan Ali <git@hasali.co.uk>
2021-05-02 16:28:58 +0100

    Implement show details screen

--- client/android/app/src/main/java/uk/hasali/zenith/MainActivity.kt
+++ client/android/app/src/main/java/uk/hasali/zenith/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.SoundEffectConstants
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
@@ -14,10 +15,8 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.produceState
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -29,6 +28,7 @@ import androidx.core.view.WindowCompat
 import com.google.accompanist.coil.rememberCoilPainter
 import com.google.accompanist.insets.ProvideWindowInsets
 import com.google.accompanist.insets.statusBarsHeight
+import com.zachklipp.compose.backstack.Backstack
 import io.ktor.client.*
 import io.ktor.client.features.json.*
 import io.ktor.client.features.json.serializer.*
@@ -44,10 +44,37 @@ data class Show(
     val id: Int,
     val name: String,
     val poster: String,
+    val backdrop: String,
+    val overview: String,
     @SerialName("start_date") val startDate: Long,
 )
 
+sealed class Screen {
+    object Shows : Screen()
+    data class ShowDetails(val show: Show) : Screen()
+}
+
+class Navigator {
+    var stack by mutableStateOf(listOf<Screen>(Screen.Shows))
+
+    fun push(screen: Screen) {
+        stack = stack + screen
+    }
+
+    fun pop(): Boolean {
+        return if (stack.size > 1) {
+            stack = stack.dropLast(1)
+            true
+        } else {
+            false
+        }
+    }
+}
+
 class MainActivity : ComponentActivity() {
+    private val navigator = Navigator()
+
+    @OptIn(ExperimentalAnimationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -57,9 +84,20 @@ class MainActivity : ComponentActivity() {
         setContent {
             AppTheme {
                 ProvideWindowInsets {
-                    ShowsScreen()
+                    Backstack(backstack = navigator.stack) { screen ->
+                        when (screen) {
+                            is Screen.Shows -> ShowsScreen(navigator = navigator)
+                            is Screen.ShowDetails -> ShowDetailsScreen(show = screen.show)
+                        }
+                    }
                 }
             }
+        }
+    }
+
+    override fun onBackPressed() {
+        if (!navigator.pop()) {
+            super.onBackPressed()
         }
     }
 }
@@ -89,7 +127,7 @@ fun AppBar() {
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun ShowsScreen() {
+fun ShowsScreen(navigator: Navigator) {
     val client = remember {
         HttpClient() {
             install(JsonFeature) {
@@ -110,14 +148,63 @@ fun ShowsScreen() {
             contentPadding = PaddingValues(4.dp),
         ) {
             items(shows.size) { i ->
-                ShowGridItem(show = shows[i])
+                ShowGridItem(
+                    show = shows[i],
+                    onClick = { navigator.push(Screen.ShowDetails(shows[i])) },
+                )
             }
         }
     }
 }
 
 @Composable
-fun ShowGridItem(show: Show) {
+fun ShowDetailsScreen(show: Show) {
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column {
+            Image(
+                painter = rememberCoilPainter(request = show.backdrop, fadeIn = true),
+                contentDescription = "Backdrop",
+                modifier = Modifier.aspectRatio(16f / 9f)
+            )
+
+            Column(modifier = Modifier.offset(y = (-48).dp)) {
+                Row(modifier = Modifier.padding(horizontal = 16.dp)) {
+                    Card {
+                        Image(
+                            painter = rememberCoilPainter(request = show.poster, fadeIn = true),
+                            contentDescription = "Poster",
+                            modifier = Modifier
+                                .width(140.dp)
+                                .aspectRatio(2f / 3f),
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.width(16.dp))
+
+                    Column(modifier = Modifier.align(Alignment.CenterVertically)) {
+                        val year = Instant.fromEpochSeconds(show.startDate)
+                            .toLocalDateTime(TimeZone.UTC)
+                            .year
+
+                        Text(show.name, style = MaterialTheme.typography.h5)
+                        Text(year.toString(), style = MaterialTheme.typography.caption)
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Text(
+                    show.overview,
+                    style = MaterialTheme.typography.body2,
+                    modifier = Modifier.padding(horizontal = 16.dp),
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun ShowGridItem(show: Show, onClick: () -> Unit) {
     val context = LocalContext.current
 
     @Composable
@@ -135,6 +222,7 @@ fun ShowGridItem(show: Show) {
                         val audioManager =
                             context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
                         audioManager.playSoundEffect(SoundEffectConstants.CLICK, 1.0f)
+                        onClick()
                     }
             )
         }
