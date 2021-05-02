42aff48fa7c4920256239e59de67521e42e06d75
Hasan Ali <git@hasali.co.uk>
2021-05-02 12:38:03 +0100

    Enable jetpack compose

--- client/android/app/src/main/java/uk/hasali/zenith/MainActivity.kt
+++ client/android/app/src/main/java/uk/hasali/zenith/MainActivity.kt
@@ -1,11 +1,63 @@
 package uk.hasali.zenith
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.core.view.WindowCompat
+import com.google.accompanist.insets.ProvideWindowInsets
+import com.google.accompanist.insets.statusBarsHeight
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        setContent {
+            AppTheme {
+                ProvideWindowInsets {
+                    Scaffold(topBar = { AppBar() }) {
+                        Box(modifier = Modifier.fillMaxSize()) {
+                            Text(
+                                text = "Hello, world!",
+                                modifier = Modifier.align(Alignment.Center),
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun AppTheme(content: @Composable () -> Unit) {
+    MaterialTheme(
+        colors = if (isSystemInDarkTheme()) darkColors() else lightColors(),
+    ) {
+        content()
+    }
+}
+
+@Composable
+fun AppBar() {
+    Surface(color = MaterialTheme.colors.primarySurface, elevation = 4.dp) {
+        Column {
+            Spacer(modifier = Modifier.statusBarsHeight())
+            TopAppBar(
+                title = { Text("Zenith") },
+                backgroundColor = Color.Transparent,
+                elevation = 0.dp,
+            )
+        }
     }
 }
