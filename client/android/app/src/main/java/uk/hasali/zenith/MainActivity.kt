39bb4bb2b31965f437b82de29d69787c1cd0caca
Hasan Ali <git@hasali.co.uk>
2021-05-02 13:45:35 +0100

    Implement shows list screen

--- client/android/app/src/main/java/uk/hasali/zenith/MainActivity.kt
+++ client/android/app/src/main/java/uk/hasali/zenith/MainActivity.kt
@@ -1,38 +1,63 @@
 package uk.hasali.zenith
 
+import android.content.Context
+import android.media.AudioManager
 import android.os.Bundle
+import android.view.SoundEffectConstants
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.produceState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
+import com.google.accompanist.coil.rememberCoilPainter
 import com.google.accompanist.insets.ProvideWindowInsets
 import com.google.accompanist.insets.statusBarsHeight
+import io.ktor.client.*
+import io.ktor.client.features.json.*
+import io.ktor.client.features.json.serializer.*
+import io.ktor.client.request.*
+import kotlinx.datetime.Instant
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Show(
+    val id: Int,
+    val name: String,
+    val poster: String,
+    @SerialName("start_date") val startDate: Long,
+)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Enable drawing under the status bar
         WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContent {
             AppTheme {
                 ProvideWindowInsets {
-                    Scaffold(topBar = { AppBar() }) {
-                        Box(modifier = Modifier.fillMaxSize()) {
-                            Text(
-                                text = "Hello, world!",
-                                modifier = Modifier.align(Alignment.Center),
-                            )
-                        }
-                    }
+                    ShowsScreen()
                 }
             }
         }
@@ -58,6 +83,95 @@ fun AppBar() {
                 backgroundColor = Color.Transparent,
                 elevation = 0.dp,
             )
+        }
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun ShowsScreen() {
+    val client = remember {
+        HttpClient() {
+            install(JsonFeature) {
+                serializer = KotlinxSerializer(kotlinx.serialization.json.Json {
+                    ignoreUnknownKeys = true
+                })
+            }
+        }
+    }
+
+    val shows by produceState(initialValue = emptyList<Show>()) {
+        value = client.get("https://zenith.hasali.uk/api/tv/shows")
+    }
+
+    Scaffold(topBar = { AppBar() }) {
+        LazyVerticalGrid(
+            cells = GridCells.Adaptive(120.dp),
+            contentPadding = PaddingValues(4.dp),
+        ) {
+            items(shows.size) { i ->
+                ShowGridItem(show = shows[i])
+            }
+        }
+    }
+}
+
+@Composable
+fun ShowGridItem(show: Show) {
+    val context = LocalContext.current
+
+    @Composable
+    fun Poster(width: Dp, height: Dp) {
+        Card {
+            Image(
+                painter = rememberCoilPainter(
+                    show.poster,
+                    fadeIn = true
+                ),
+                contentDescription = "Poster",
+                modifier = Modifier
+                    .size(width, height)
+                    .clickable {
+                        val audioManager =
+                            context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+                        audioManager.playSoundEffect(SoundEffectConstants.CLICK, 1.0f)
+                    }
+            )
+        }
+    }
+
+    @Composable
+    fun Content() {
+        val year = Instant.fromEpochSeconds(show.startDate)
+            .toLocalDateTime(TimeZone.UTC)
+            .year
+
+        Column(modifier = Modifier.padding(vertical = 4.dp)) {
+            Text(
+                show.name,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                style = MaterialTheme.typography.subtitle2
+            )
+
+            Text(
+                year.toString(),
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                style = MaterialTheme.typography.caption
+            )
+        }
+    }
+
+    BoxWithConstraints(modifier = Modifier.padding(4.dp)) {
+        with(LocalDensity.current) {
+            val width = constraints.maxWidth
+            val height = width * 1.5
+
+            Column {
+                Poster(width = width.toDp(), height = height.toInt().toDp())
+                Content()
+            }
         }
     }
 }
