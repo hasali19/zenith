fb3ff61be5689581c4cde3f4dbb138c8a513235b
Hasan Ali <hasan@hasali.dev>
2021-05-22 00:27:14 +0100

    [rn] Install react native paper

--- client/react-native/App.tsx
+++ client/react-native/App.tsx
@@ -1,18 +1,23 @@
 import React, { FC } from "react";
-import { Text, useColorScheme, View } from "react-native";
+import { useColorScheme, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
-import { NavigationContainer, useTheme } from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { Button, Provider as PaperProvider, Text } from "react-native-paper";
 import { navigationTheme } from "./theme";
 
-const HomeScreen: FC = () => {
-  const theme = useTheme();
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text style={{ color: theme.colors.text }}>Home Screen</Text>
-    </View>
-  );
-};
+const HomeScreen: FC = () => (
+  <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <Text>Home Screen</Text>
+    <Button
+      mode="contained"
+      style={{ margin: 8 }}
+      onPress={() => console.log("Hello, world!")}
+    >
+      Click Me
+    </Button>
+  </View>
+);
 
 const Stack = createStackNavigator();
 
@@ -20,12 +25,14 @@ const App: FC = () => {
   const scheme = useColorScheme();
   const theme = navigationTheme(scheme);
   return (
-    <NavigationContainer theme={theme}>
-      <StatusBar />
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <PaperProvider>
+      <NavigationContainer theme={theme}>
+        <StatusBar />
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 };
 
