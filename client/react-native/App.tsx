f5b255f457b1d2471d8412bc480941282674e9cd
Hasan Ali <hasan@hasali.dev>
2021-05-22 00:08:23 +0100

    [rn] Install react navigation

--- client/react-native/App.tsx
+++ client/react-native/App.tsx
@@ -1,21 +1,24 @@
+import React, { FC } from "react";
+import { Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const HomeScreen: FC = () => (
+  <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <Text>Home Screen</Text>
+  </View>
+);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const Stack = createStackNavigator();
+
+const App: FC = () => (
+  <NavigationContainer>
+    <StatusBar />
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
+
+export default App;
