0d206bb29d3e914d55a6f8855282f460b10cf0f6
Hasan Ali <hasan@hasali.dev>
2021-06-08 19:27:16 +0100

    [web] Improve movie details screen on desktop

--- client/web/src/pages/Movie.tsx
+++ client/web/src/pages/Movie.tsx
@@ -1,12 +1,19 @@
-import { useEffect, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router";
 import { css, Theme } from "@emotion/react";
-import { Button, Icon, LinearProgress, Typography } from "@material-ui/core";
+import {
+  Button,
+  Card,
+  CardMedia,
+  Icon,
+  LinearProgress,
+  Typography,
+  useMediaQuery,
+} from "@material-ui/core";
 
 import api, { Movie, VideoInfo } from "../api";
-import MediaInfo from "../components/MediaInfo";
-import SubtitleSelect from "../components/SubtitleSelect";
 import AppBar from "../AppBar";
+import MediaInfo from "../components/MediaInfo";
 
 function displayDuration(duration: number) {
   if (duration <= 90 * 60) {
@@ -18,42 +25,13 @@ function displayDuration(duration: number) {
   }
 }
 
-const styles = {
-  root: css`
-    height: 100%;
-    overflow-y: auto;
-  `,
-
-  backdrop: css`
-    width: 100%;
-    aspect-ratio: 16 / 9;
-  `,
-
-  title: (theme: Theme) => css`
-    margin: 0 ${theme.spacing(2)};
-    margin-top: ${theme.spacing(3)};
-  `,
-
-  subtitle: (theme: Theme) => css`
-    margin: 0 ${theme.spacing(2)};
-  `,
-
-  play: (theme: Theme) => css`
-    margin: ${theme.spacing(2)};
-  `,
-
-  overview: (theme: Theme) =>
-    css`
-      margin: 0 ${theme.spacing(2)};
-    `,
-};
-
 export default function () {
   const params = useParams<any>();
   const history = useHistory();
+  const mobile = useMediaQuery((theme: Theme) => theme.breakpoints.down("md"));
+
   const [movie, setMovie] = useState<Movie | null>(null);
   const [video, setVideo] = useState<VideoInfo | null>(null);
-  const [subtitle, setSubtitle] = useState<"none" | number>("none");
 
   useEffect(() => {
     api.movies.getMovie(params.id).then(setMovie);
@@ -64,61 +42,172 @@ export default function () {
     return <LinearProgress variant="indeterminate" />;
   }
 
-  function onPlay() {
-    if (movie) {
-      let url = `/player/${movie.id}`;
-
-      if (subtitle !== "none") {
-        url += `?subtitle=${subtitle}`;
-      }
-
-      history.push(url);
-    }
-  }
-
   return (
-    <div css={styles.root}>
-      <AppBar translucent />
-      <img src={movie.backdrop!!} css={styles.backdrop} />
-      <Typography variant="h4" css={styles.title}>
-        {movie.title}
-      </Typography>
-      <Typography variant="caption" component="div" css={styles.subtitle}>
-        {movie.releaseYear()} • {displayDuration(movie.duration)}
-      </Typography>
-      <Button
-        variant="contained"
-        startIcon={<Icon>play_arrow</Icon>}
-        css={styles.play}
-        onClick={onPlay}
-      >
-        Play
-      </Button>
+    <div
+      css={css`
+        height: 100%;
+        overflow-y: auto;
+        background-size: cover;
+        background-position: center;
+      `}
+      style={{
+        backgroundImage: mobile ? undefined : `url(${movie.backdrop})`,
+      }}
+    >
       <div
         css={(theme) => css`
-          margin: 0 ${theme.spacing(2)};
-          margin-bottom: ${theme.spacing(2)};
+          height: 100%;
+          overflow-y: auto;
+
+          ${theme.breakpoints.up("md")} {
+            padding: 10%;
+            background-color: #000a;
+          }
         `}
       >
-        <SubtitleSelect
-          subtitles={video.subtitles}
-          value={subtitle}
-          onChange={setSubtitle}
+        <AppBar translucent />
+        {mobile && movie.backdrop && <MobileBackdrop src={movie.backdrop} />}
+        <HeaderSection
+          movie={movie}
+          mobile={mobile}
+          onPlay={(player) => history.push(`/${player}/${movie.id}`)}
         />
-      </div>
-      <Typography variant="body2" css={styles.overview}>
-        {movie.overview}
-      </Typography>
-      <div
-        css={(theme) =>
-          css`
-            margin-top: ${theme.spacing(2)};
-            padding: 0 ${theme.spacing(2)};
-          `
-        }
-      >
-        <MediaInfo info={video} />
+        {mobile && (
+          <Typography
+            variant="body2"
+            css={(theme) => css`
+              margin: 0 ${theme.spacing(2)};
+            `}
+          >
+            {movie.overview}
+          </Typography>
+        )}
+        <div
+          css={(theme) =>
+            css`
+              margin-top: ${theme.spacing(2)};
+
+              ${theme.breakpoints.up("md")} {
+                margin-top: ${theme.spacing(4)};
+              }
+
+              ${theme.breakpoints.down("md")} {
+                padding: 0 ${theme.spacing(2)};
+              }
+            `
+          }
+        >
+          <MediaInfo info={video} />
+        </div>
       </div>
     </div>
   );
 }
+
+const MobileBackdrop: FC<{ src: string }> = ({ src }) => (
+  <img
+    src={src}
+    css={css`
+      width: 100%;
+      aspect-ratio: 16 / 9;
+    `}
+  />
+);
+
+const HeaderSection: FC<{
+  movie: Movie;
+  mobile: boolean;
+  onPlay: (player: "player" | "cast") => void;
+}> = ({ movie, mobile, onPlay }) => (
+  <div
+    css={(theme) => css`
+      display: flex;
+      align-items: center;
+
+      ${theme.breakpoints.down("md")} {
+        align-items: flex-end;
+        padding: ${theme.spacing(2)};
+        margin-top: -80px;
+      }
+    `}
+  >
+    <Card
+      css={(theme) => css`
+        min-width: 150px;
+
+        ${theme.breakpoints.up("md")} {
+          min-width: 280px;
+        }
+      `}
+    >
+      <CardMedia
+        image={movie.poster!!}
+        css={css`
+          aspect-ratio: 2/3;
+        `}
+      />
+    </Card>
+    <div
+      css={(theme) => css`
+        margin-left: ${theme.spacing(2)};
+
+        ${theme.breakpoints.up("md")} {
+          margin-left: ${theme.spacing(4)};
+        }
+      `}
+    >
+      <Typography
+        variant={mobile ? "h5" : "h3"}
+        css={(theme) => css`
+          ${theme.breakpoints.down("md")} {
+            font-size: 1.2rem;
+          }
+        `}
+      >
+        {movie.title}
+      </Typography>
+      <Typography variant={mobile ? "caption" : "h6"} component="div">
+        {displayDuration(movie.duration)}
+      </Typography>
+      <ActionsRow onPlay={onPlay} />
+      {!mobile && (
+        <Typography
+          variant="body2"
+          css={(theme) => css`
+            margin-top: ${theme.spacing(2)};
+          `}
+        >
+          {movie.overview}
+        </Typography>
+      )}
+    </div>
+  </div>
+);
+
+const ActionsRow: FC<{ onPlay: (player: "player" | "cast") => void }> = ({
+  onPlay,
+}) => {
+  return (
+    <div
+      css={(theme) =>
+        css`
+          margin: ${theme.spacing(2)} 0;
+        `
+      }
+    >
+      <Button
+        variant="contained"
+        startIcon={<Icon>play_arrow</Icon>}
+        onClick={() => onPlay("player")}
+        css={(theme) => css`
+          margin-right: ${theme.spacing(1)};
+        `}
+      >
+        Play
+      </Button>
+      <Button variant="outlined" onClick={() => onPlay("cast")}>
+        <Icon color="action">cast</Icon>
+      </Button>
+    </div>
+  );
+};
