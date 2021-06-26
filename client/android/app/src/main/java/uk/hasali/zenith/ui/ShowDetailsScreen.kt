127709840d6df1e00c5e8b0c6d9d59ebd994cab5
Hasan Ali <hasan@hasali.dev>
2021-06-26 17:02:06 +0100

    [android] Use ContentScale.Crop for backdrop images

--- client/android/app/src/main/java/uk/hasali/zenith/ui/ShowDetailsScreen.kt
+++ client/android/app/src/main/java/uk/hasali/zenith/ui/ShowDetailsScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.produceState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.coil.rememberCoilPainter
@@ -44,6 +45,7 @@ fun ShowDetailsScreen(show: Show) {
             Image(
                 painter = rememberCoilPainter(request = show.backdrop, fadeIn = true),
                 contentDescription = "Backdrop",
+                contentScale = ContentScale.Crop,
                 modifier = Modifier.aspectRatio(16f / 9f)
             )
 
