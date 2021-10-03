7c35e1bade047be5e4fd8a554933ef2c0ebc446e
Hasan Ali <hasan@hasali.dev>
2021-10-03 18:30:33 +0100

    [server] Add endpoint to rescan specific video

--- zenith/src/api/scanner.rs
+++ zenith/src/api/scanner.rs
@@ -1,7 +1,7 @@
 use std::sync::Arc;
 
 use atium::respond::RespondRequestExt;
-use atium::router::Router;
+use atium::router::{Router, RouterRequestExt};
 use atium::{endpoint, Request, StatusCode};
 
 use crate::library::scanner::ScanOptions;
@@ -9,6 +9,7 @@ use crate::library::LibraryScanner;
 
 pub fn routes(router: &mut Router) {
     router.route("/scanner/start").post(start_scan);
+    router.route("/scanner/run/:id").post(scan_item);
 }
 
 #[endpoint]
@@ -20,6 +21,17 @@ async fn start_scan(req: &mut Request) -> eyre::Result<()> {
         refresh_metadata: false,
     });
 
+    req.respond(StatusCode::OK);
+
+    Ok(())
+}
+
+#[endpoint]
+async fn scan_item(req: &mut Request) -> eyre::Result<()> {
+    let id: i64 = req.param("id")?;
+    let scanner: &Arc<LibraryScanner> = req.ext().unwrap();
+
+    scanner.rescan_video_file(id).await?;
     req.respond(StatusCode::OK);
 
     Ok(())
