af9a9eab2bbeb32a9413641f91353e012658a596
Hasan Ali <hasan@hasali.dev>
2021-10-16 21:47:11 +0100

    [server] Add endpoint for fetching multiple items by ids

--- zenith/src/api/items.rs
+++ zenith/src/api/items.rs
@@ -1,33 +1,39 @@
+use atium::query::QueryRequestExt;
 use atium::respond::RespondRequestExt;
 use atium::router::{Router, RouterRequestExt};
 use atium::{endpoint, Request};
-use serde::{Deserialize, Serialize};
-use sqlx::SqliteConnection;
+use serde::Deserialize;
 
 use crate::api::error::bad_request;
-use crate::db::episodes::Episode;
 use crate::db::media::MediaItemType;
-use crate::db::movies::Movie;
-use crate::db::seasons::Season;
-use crate::db::shows::Show;
 use crate::db::videos::UpdateVideoUserData;
 use crate::db::{self, Db};
 
 use super::ext::OptionExt;
 
 pub fn routes(router: &mut Router) {
+    router.route("/items").get(get_items);
     router.route("/items/:id").get(get_item);
     router.route("/items/:id/user_data").patch(update_user_data);
 }
 
-#[derive(Serialize)]
-#[serde(tag = "type")]
-#[serde(rename_all = "snake_case")]
-enum MediaItem {
-    Movie(Movie),
-    Show(Show),
-    Season(Season),
-    Episode(Episode),
+#[derive(Deserialize)]
+struct GetItemsQuery {
+    #[serde(default)]
+    ids: Vec<i64>,
+}
+
+#[endpoint]
+async fn get_items(req: &mut Request) -> eyre::Result<()> {
+    let query: GetItemsQuery = req.query()?;
+    let db: &Db = req.ext().unwrap();
+    let mut conn = db.acquire().await?;
+
+    let items = db::items::get_multiple(&mut conn, query.ids).await?;
+
+    req.ok().json(&items)?;
+
+    Ok(())
 }
 
 #[endpoint]
@@ -36,32 +42,13 @@ async fn get_item(req: &mut Request) -> eyre::Result<()> {
     let db: &Db = req.ext().unwrap();
     let mut conn = db.acquire().await?;
 
-    let item_type = db::media::get_item_type(&mut conn, id)
-        .await?
-        .or_not_found("media item not found")?;
-
-    let item = get_media_item(&mut conn, id, item_type)
+    let item = db::items::get(&mut conn, id)
         .await?
         .or_not_found("media item not found")?;
 
     req.ok().json(&item)?;
 
     Ok(())
-}
-
-async fn get_media_item(
-    conn: &mut SqliteConnection,
-    id: i64,
-    item_type: MediaItemType,
-) -> eyre::Result<Option<MediaItem>> {
-    let item = match item_type {
-        MediaItemType::Movie => db::movies::get(conn, id).await?.map(MediaItem::Movie),
-        MediaItemType::TvShow => db::shows::get(conn, id).await?.map(MediaItem::Show),
-        MediaItemType::TvSeason => db::seasons::get(conn, id).await?.map(MediaItem::Season),
-        MediaItemType::TvEpisode => db::episodes::get(conn, id).await?.map(MediaItem::Episode),
-    };
-
-    Ok(item)
 }
 
 #[derive(Deserialize)]
--- zenith/src/db/items.rs
+++ zenith/src/db/items.rs
@@ -1,6 +1,55 @@
 use serde::Serialize;
+use sqlx::SqliteConnection;
+
+use super::episodes::{self, Episode};
+use super::media::{self, MediaItemType};
+use super::movies::{self, Movie};
+use super::seasons::{self, Season};
+use super::shows::{self, Show};
 
 #[derive(Serialize)]
 pub struct ExternalIds {
     pub tmdb: Option<i32>,
+}
+
+#[derive(Serialize)]
+#[serde(tag = "type")]
+#[serde(rename_all = "snake_case")]
+pub enum MediaItem {
+    Movie(Movie),
+    Show(Show),
+    Season(Season),
+    Episode(Episode),
+}
+
+pub async fn get(conn: &mut SqliteConnection, id: i64) -> eyre::Result<Option<MediaItem>> {
+    let item_type = match media::get_item_type(&mut *conn, id).await? {
+        Some(item_type) => item_type,
+        None => return Ok(None),
+    };
+
+    let item = match item_type {
+        MediaItemType::Movie => movies::get(&mut *conn, id).await?.map(MediaItem::Movie),
+        MediaItemType::TvShow => shows::get(&mut *conn, id).await?.map(MediaItem::Show),
+        MediaItemType::TvSeason => seasons::get(&mut *conn, id).await?.map(MediaItem::Season),
+        MediaItemType::TvEpisode => episodes::get(&mut *conn, id).await?.map(MediaItem::Episode),
+    };
+
+    Ok(item)
+}
+
+pub async fn get_multiple(
+    conn: &mut SqliteConnection,
+    ids: impl IntoIterator<Item = i64>,
+) -> eyre::Result<Vec<MediaItem>> {
+    // TODO: Reduce number of db queries
+    let mut items = vec![];
+
+    for id in ids {
+        if let Some(item) = get(&mut *conn, id).await? {
+            items.push(item);
+        }
+    }
+
+    Ok(items)
 }
