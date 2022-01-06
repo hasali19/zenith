6d31fa33776a4bbd889d06adce890bc2d35787fd
Hasan Ali <hasan@hasali.dev>
2022-01-06 14:45:29 +0000

    [server] Add option to disable rescan_files

--- server/zenith/src/api/scanner.rs
+++ server/zenith/src/api/scanner.rs
@@ -1,26 +1,38 @@
 use std::sync::Arc;
 
-use axum::extract::{Extension, Path};
+use axum::extract::{Extension, Path, Query};
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use axum_codegen::post;
+use serde::Deserialize;
 
 use crate::api::ApiResult;
 use crate::library::scanner::ScanOptions;
 use crate::library::LibraryScanner;
 
+#[derive(Deserialize)]
+struct StartScanQuery {
+    #[serde(default)]
+    rescan_files: bool,
+    #[serde(default)]
+    refresh_metadata: bool,
+}
+
 #[post("/scanner/start")]
-pub async fn start_scan(Extension(scanner): Extension<Arc<LibraryScanner>>) -> impl IntoResponse {
+async fn start_scan(
+    Query(query): Query<StartScanQuery>,
+    Extension(scanner): Extension<Arc<LibraryScanner>>,
+) -> impl IntoResponse {
     scanner.start_scan(ScanOptions {
-        rescan_files: true,
-        refresh_metadata: false,
+        rescan_files: query.rescan_files,
+        refresh_metadata: query.refresh_metadata,
     });
 
     StatusCode::OK
 }
 
 #[post("/scanner/run/:id")]
-pub async fn scan_item(
+async fn scan_item(
     Path(id): Path<i64>,
     Extension(scanner): Extension<Arc<LibraryScanner>>,
 ) -> ApiResult<impl IntoResponse> {
