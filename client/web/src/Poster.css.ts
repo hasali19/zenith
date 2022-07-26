f25a0a11b0f930104afac97947ca21c06e79e3dc
Hasan Ali <hasan@hasali.dev>
2022-07-26 17:03:36 +0100

    [web] Fix layout of fallback posters

--- client/web/src/Poster.css.ts
+++ client/web/src/Poster.css.ts
@@ -2,8 +2,14 @@ import { style } from "@vanilla-extract/css";
 
 export const root = style({
   position: "relative",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
   userSelect: "none",
   transition: "transform 100ms",
+  background: "rgba(100,100,100)",
+  borderRadius: 8,
+  aspectRatio: "2/3",
 });
 
 export const image = style({
