1b68bbd64128ec811a51228853559ce28986658d
Hasan Ali <hasan@hasali.dev>
2022-09-11 14:42:27 +0100

    [server] Add test cases for items api

--- server/zenith/test/media.rs
+++ server/zenith/test/media.rs
@@ -4,7 +4,7 @@ use test_macros::test;
 use crate::{with_app, TestApp};
 
 macro_rules! test_snapshot {
-    ($name:ident, $path:expr) => {
+    ($name:ident, $path:expr $(,)?) => {
         #[test(with_app)]
         async fn $name(app: TestApp) {
             assert_json_snapshot!(app.get($path).await);
@@ -21,3 +21,8 @@ test_snapshot!(get_season_by_id, "/tv/seasons/7");
 test_snapshot!(get_episodes_for_show, "/tv/shows/4/episodes");
 test_snapshot!(get_episodes_for_season, "/tv/seasons/7/episodes");
 test_snapshot!(get_episode_by_id, "/tv/episodes/9");
+test_snapshot!(get_single_item_by_id, "/items/1");
+test_snapshot!(
+    get_multiple_items_by_ids,
+    "/items?ids[]=1&ids[]=4&ids[]=7&ids[]=9",
+);
