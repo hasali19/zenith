67ab872cda095c2325604015f2f1efcfe8fcd26d
Hasan Ali <hasan@hasali.dev>
2022-11-19 17:54:47 +0000

    [flutter] Implement main media controls on windows

--- flutter/video_player/video_player_ffi/lib/video_player_ffi.dart
+++ flutter/video_player/video_player_ffi/lib/video_player_ffi.dart
@@ -1,39 +1,49 @@
 import 'dart:ffi';
+import 'dart:isolate';
 
 import 'package:ffi/ffi.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_native_view/flutter_native_view.dart';
 import 'package:video_player_platform_interface/video_player_platform_interface.dart';
 
-// const _channel = MethodChannel("video_player_ffi");
-
 final DynamicLibrary _lib = DynamicLibrary.open("video_player_ffi.dll");
-final int Function() ffiCreateWindow = _lib
-    .lookup<NativeFunction<IntPtr Function()>>("create_window")
+final int Function(int nativePort, Pointer<Void> params) ffiCreatePlayer = _lib
+    .lookup<NativeFunction<IntPtr Function(Int64, Pointer<Void>)>>(
+        "create_player")
     .asFunction();
-final int Function(int hwnd) ffiCreatePlayer = _lib
-    .lookup<NativeFunction<IntPtr Function(IntPtr)>>("create_player")
+final int Function(int player) ffiGetWindowHandle = _lib
+    .lookup<NativeFunction<IntPtr Function(IntPtr)>>("get_window_handle")
     .asFunction();
-final int Function(int hwnd, Pointer<Utf8> url) ffiLoad = _lib
-    .lookup<NativeFunction<IntPtr Function(IntPtr, Pointer<Utf8>)>>("load")
+final void Function(int player, Pointer<Utf8> url, double startPosition)
+    ffiLoad = _lib
+        .lookup<NativeFunction<Void Function(IntPtr, Pointer<Utf8>, Double)>>(
+            "load")
+        .asFunction();
+final void Function(int player) ffiPause =
+    _lib.lookup<NativeFunction<Void Function(IntPtr)>>("pause").asFunction();
+final void Function(int player) ffiPlay =
+    _lib.lookup<NativeFunction<Void Function(IntPtr)>>("play").asFunction();
+final void Function(int player, double position) ffiSeekTo = _lib
+    .lookup<NativeFunction<Void Function(IntPtr, Double)>>("seek_to")
     .asFunction();
-final int Function(int hwnd) ffiPause =
-    _lib.lookup<NativeFunction<IntPtr Function(IntPtr)>>("pause").asFunction();
-final int Function(int hwnd) ffiPlay =
-    _lib.lookup<NativeFunction<IntPtr Function(IntPtr)>>("play").asFunction();
+final void Function(int player) ffiDestroyPlayer = _lib
+    .lookup<NativeFunction<Void Function(IntPtr)>>("destroy_player")
+    .asFunction();
 
 class VideoPlayerFfi extends VideoPlayerPlatform {
   static registerWith() {
     VideoPlayerPlatform.instance = VideoPlayerFfi();
   }
 
+  bool _flutterNativeViewInitialized = false;
+
   @override
   Future<VideoController> createController() async {
-    FlutterNativeView.ensureInitialized();
-    final hwnd = ffiCreateWindow();
-    final player = ffiCreatePlayer(hwnd);
-    return VideoControllerWindows(hwnd, player);
+    if (!_flutterNativeViewInitialized) {
+      FlutterNativeView.ensureInitialized();
+      _flutterNativeViewInitialized = true;
+    }
+    return VideoControllerWindows();
   }
 
   @override
@@ -67,51 +77,106 @@ class VideoPlayerFfi extends VideoPlayerPlatform {
   }
 }
 
+enum PlayerMsgKind {
+  durationChanged,
+  pausedChanged,
+  idleChanged,
+  videoEnded,
+}
+
 class VideoControllerWindows extends VideoController {
-  final int hwnd;
-  final int player;
-  final NativeViewController c;
+  late final NativeViewController c;
+  late final int player;
 
-  VideoControllerWindows(this.hwnd, this.player)
-      : c = NativeViewController(handle: hwnd);
+  final port = ReceivePort();
+
+  VideoControllerWindows() {
+    port.listen((message) {
+      List<dynamic> values = message;
+
+      final double position = values[0];
+      final kind = PlayerMsgKind.values[values[1] - 1];
+      final dynamic data = values[2];
+
+      _lastKnownPosition = position * 1000;
+      _lastKnownPositionTs = DateTime.now().millisecondsSinceEpoch;
+
+      switch (kind) {
+        case PlayerMsgKind.durationChanged:
+          _duration = data;
+          break;
+        case PlayerMsgKind.pausedChanged:
+          _paused = data;
+          break;
+        case PlayerMsgKind.idleChanged:
+          _playing = !data;
+          break;
+        case PlayerMsgKind.videoEnded:
+          _state = VideoState.ended;
+          break;
+      }
+
+      _notifyListeners();
+    });
+
+    player = ffiCreatePlayer(
+        port.sendPort.nativePort, NativeApi.initializeApiDLData);
+
+    c = NativeViewController(handle: ffiGetWindowHandle(player));
+  }
+
+  double _lastKnownPosition = 0;
+  int _lastKnownPositionTs = DateTime.now().millisecondsSinceEpoch;
+  bool _playing = false;
 
   @override
-  double position = 0;
-
-  @override
-  void dispose() {
-    // _channel.invokeMethod("DestroyPlayer", player);
+  double get position {
+    var position = _lastKnownPosition;
+    if (_playing) {
+      position += DateTime.now().millisecondsSinceEpoch - _lastKnownPositionTs;
+    }
+    return position / 1000;
   }
 
   @override
-  // TODO: implement duration
-  double get duration => 0;
+  set position(value) {
+    ffiSeekTo(player, value);
+  }
+
+  @override
+  void dispose() {
+    ffiDestroyPlayer(player);
+    port.close();
+  }
+
+  @override
+  double get duration => _duration;
+  double _duration = 0;
 
   @override
   void load(String url, List<SubtitleTrack> subtitles, double startPosition) {
     final pUrl = url.toNativeUtf8();
-    ffiLoad(player, pUrl);
+    ffiLoad(player, pUrl, startPosition);
     calloc.free(pUrl);
+    _state = VideoState.active;
   }
 
   @override
-  // TODO: implement loading
-  bool get loading => false;
+  bool get loading => !paused && !_playing && state != VideoState.ended;
 
   @override
   void pause() {
     ffiPause(player);
-    paused = true;
     _notifyListeners();
   }
 
   @override
-  bool paused = false;
+  bool get paused => _paused;
+  bool _paused = false;
 
   @override
   void play() {
     ffiPlay(player);
-    paused = false;
     _notifyListeners();
   }
 
@@ -126,8 +191,8 @@ class VideoControllerWindows extends VideoController {
   }
 
   @override
-  // TODO: implement state
-  VideoState get state => VideoState.active;
+  VideoState get state => _state;
+  VideoState _state = VideoState.idle;
 
   final List<void Function()> _listeners = [];
 
--- flutter/video_player/video_player_ffi/src/lib.rs
+++ flutter/video_player/video_player_ffi/src/lib.rs
@@ -1,18 +1,26 @@
-use std::sync::mpsc;
+#![allow(clippy::missing_safety_doc)]
+
+mod window;
+
+use std::ffi::{c_void, CStr};
+use std::sync::atomic::{self, AtomicBool, AtomicU64};
+use std::sync::Arc;
 
 use mpv::{
-    mpv_command, mpv_create, mpv_format_MPV_FORMAT_FLAG, mpv_format_MPV_FORMAT_INT64, mpv_handle,
-    mpv_initialize, mpv_set_option, mpv_set_property,
+    mpv_command, mpv_create, mpv_event_id_MPV_EVENT_END_FILE,
+    mpv_event_id_MPV_EVENT_PLAYBACK_RESTART, mpv_event_id_MPV_EVENT_PROPERTY_CHANGE,
+    mpv_event_id_MPV_EVENT_SHUTDOWN, mpv_event_id_MPV_EVENT_START_FILE, mpv_event_property,
+    mpv_format_MPV_FORMAT_DOUBLE, mpv_format_MPV_FORMAT_FLAG, mpv_format_MPV_FORMAT_INT64,
+    mpv_get_property, mpv_handle, mpv_initialize, mpv_observe_property, mpv_set_option,
+    mpv_set_property, mpv_terminate_destroy, mpv_wait_event, mpv_wakeup,
 };
-use windows::w;
-use windows::Win32::Foundation::{COLORREF, HWND, LPARAM, LRESULT, WPARAM};
-use windows::Win32::Graphics::Gdi::CreateSolidBrush;
-use windows::Win32::System::LibraryLoader::GetModuleHandleW;
-use windows::Win32::System::Threading::{GetStartupInfoW, STARTUPINFOW};
-use windows::Win32::UI::WindowsAndMessaging::{
-    CreateWindowExW, DefWindowProcW, DispatchMessageW, GetMessageW, LoadCursorW, LoadIconW,
-    PostQuitMessage, RegisterClassW, TranslateMessage, CS_HREDRAW, CS_VREDRAW, CW_USEDEFAULT,
-    IDC_ARROW, IDI_APPLICATION, MSG, WINDOW_EX_STYLE, WM_DESTROY, WNDCLASSW, WS_OVERLAPPEDWINDOW,
+use windows::Win32::Foundation::HWND;
+
+use dart_sdk::{
+    Dart_CObject_Type_Dart_CObject_kArray, Dart_CObject_Type_Dart_CObject_kBool,
+    Dart_CObject_Type_Dart_CObject_kDouble, Dart_CObject_Type_Dart_CObject_kInt64,
+    Dart_InitializeApiDL, Dart_PostCObject_DL, _Dart_CObject, _Dart_CObject__bindgen_ty_1,
+    _Dart_CObject__bindgen_ty_1__bindgen_ty_3,
 };
 
 macro_rules! s {
@@ -21,95 +29,229 @@ macro_rules! s {
     }};
 }
 
-#[no_mangle]
-pub unsafe extern "C" fn create_window() -> HWND {
-    let (tx, rx) = mpsc::channel();
-
-    std::thread::spawn(move || unsafe {
-        let mut startup_info = STARTUPINFOW::default();
-        GetStartupInfoW(&mut startup_info);
-
-        unsafe extern "system" fn window_proc(
-            window: HWND,
-            message: u32,
-            wparam: WPARAM,
-            lparam: LPARAM,
-        ) -> LRESULT {
-            if message == WM_DESTROY {
-                PostQuitMessage(0);
-                return LRESULT(0);
-            }
-            DefWindowProcW(window, message, wparam, lparam)
-        }
-
-        let instance = GetModuleHandleW(None).unwrap();
-        let window_class = WNDCLASSW {
-            lpszClassName: w!("DART_VLC_WINDOW"),
-            style: CS_HREDRAW | CS_VREDRAW,
-            lpfnWndProc: Some(window_proc),
-            cbClsExtra: 0,
-            cbWndExtra: 0,
-            hInstance: instance,
-            hIcon: LoadIconW(None, IDI_APPLICATION).unwrap(),
-            hCursor: LoadCursorW(None, IDC_ARROW).unwrap(),
-            hbrBackground: CreateSolidBrush(COLORREF(0)),
-            ..Default::default()
-        };
-
-        RegisterClassW(&window_class);
-
-        let window = CreateWindowExW(
-            WINDOW_EX_STYLE::default(),
-            w!("DART_VLC_WINDOW"),
-            w!("dart_vlc.instance"),
-            WS_OVERLAPPEDWINDOW,
-            CW_USEDEFAULT,
-            CW_USEDEFAULT,
-            CW_USEDEFAULT,
-            CW_USEDEFAULT,
-            None,
-            None,
-            instance,
-            None,
-        );
-
-        tx.send(window).unwrap();
-
-        let mut msg = MSG::default();
-        while GetMessageW(&mut msg, window, 0, 0).as_bool() {
-            TranslateMessage(&msg);
-            DispatchMessageW(&msg);
-        }
-    });
-
-    rx.recv().unwrap()
+pub struct Player {
+    hwnd: HWND,
+    ctx: *mut mpv_handle,
+    start_position: AtomicU64,
+    quit: AtomicBool,
 }
 
+unsafe impl Send for Player {}
+unsafe impl Sync for Player {}
+
 #[no_mangle]
-pub unsafe extern "C" fn create_player(hwnd: HWND) -> *mut mpv_handle {
+pub unsafe extern "C" fn create_player(
+    native_port: i64,
+    dart_params: *mut c_void,
+) -> *const Player {
+    if Dart_InitializeApiDL(dart_params) < 0 {
+        panic!("failed to initialize dart api");
+    }
+
+    let hwnd = window::create();
     let ctx = mpv_create();
+
     mpv_set_option(
         ctx,
         s!("wid"),
         mpv_format_MPV_FORMAT_INT64,
         &mut hwnd.clone() as *mut _ as *mut _,
     );
+
     mpv_initialize(ctx);
-    ctx
+
+    mpv_observe_property(ctx, 0, s!("duration"), mpv_format_MPV_FORMAT_DOUBLE);
+    mpv_observe_property(ctx, 0, s!("pause"), mpv_format_MPV_FORMAT_FLAG);
+    mpv_observe_property(ctx, 0, s!("core-idle"), mpv_format_MPV_FORMAT_FLAG);
+
+    let player = Arc::new(Player {
+        ctx,
+        hwnd,
+        start_position: AtomicU64::new(0),
+        quit: AtomicBool::new(false),
+    });
+
+    std::thread::spawn({
+        let player = player.clone();
+        move || unsafe { run_player_event_loop(player, native_port) }
+    });
+
+    Arc::into_raw(player)
+}
+
+unsafe fn run_player_event_loop(player: Arc<Player>, native_port: i64) {
+    let ctx = player.ctx as *mut mpv_handle;
+    let mut is_start = false;
+    loop {
+        let event = mpv_wait_event(ctx, -1.0);
+        if player.quit.load(atomic::Ordering::SeqCst) {
+            break;
+        }
+
+        let mut position = 0f64;
+        unsafe {
+            mpv_get_property(
+                ctx,
+                s!("time-pos"),
+                mpv_format_MPV_FORMAT_DOUBLE,
+                &mut position as *mut f64 as *mut c_void,
+            );
+        }
+
+        let event_id = unsafe { (*event).event_id };
+        #[allow(non_upper_case_globals)]
+        match event_id {
+            mpv_event_id_MPV_EVENT_SHUTDOWN => break,
+            mpv_event_id_MPV_EVENT_START_FILE => {
+                is_start = true;
+            }
+            mpv_event_id_MPV_EVENT_END_FILE => {
+                send_msg(
+                    native_port,
+                    &PlayerMsg {
+                        position,
+                        kind: PlayerMsgKind::VideoEnded,
+                    },
+                );
+            }
+            mpv_event_id_MPV_EVENT_PLAYBACK_RESTART => {
+                if is_start {
+                    is_start = false;
+                    seek_to(
+                        player.as_ref() as *const Player,
+                        f64::from_bits(player.start_position.swap(0, atomic::Ordering::SeqCst)),
+                    );
+                }
+            }
+            mpv_event_id_MPV_EVENT_PROPERTY_CHANGE => {
+                let data = unsafe { (*event).data as *mut mpv_event_property };
+                let name = unsafe { CStr::from_ptr((*data).name) };
+                if name.to_bytes() == b"duration" {
+                    let value = unsafe { ((*data).data as *mut f64).as_ref() };
+                    if let Some(duration) = value {
+                        send_msg(
+                            native_port,
+                            &PlayerMsg {
+                                position,
+                                kind: PlayerMsgKind::DurationChanged(*duration),
+                            },
+                        );
+                    }
+                } else if name.to_bytes() == b"pause" {
+                    let value = unsafe { ((*data).data as *mut bool).as_ref() };
+                    if let Some(paused) = value {
+                        send_msg(
+                            native_port,
+                            &PlayerMsg {
+                                position,
+                                kind: PlayerMsgKind::PauseChanged(*paused),
+                            },
+                        );
+                    }
+                } else if name.to_bytes() == b"core-idle" {
+                    let value = unsafe { ((*data).data as *mut bool).as_ref() };
+                    if let Some(idle) = value {
+                        send_msg(
+                            native_port,
+                            &PlayerMsg {
+                                position,
+                                kind: PlayerMsgKind::IdleChanged(*idle),
+                            },
+                        );
+                    }
+                }
+            }
+            _ => {}
+        }
+    }
+    mpv_terminate_destroy(player.ctx);
+    window::close(player.hwnd);
+}
+
+fn cobject_i64(value: i64) -> _Dart_CObject {
+    _Dart_CObject {
+        type_: Dart_CObject_Type_Dart_CObject_kInt64,
+        value: _Dart_CObject__bindgen_ty_1 { as_int64: value },
+    }
+}
+
+fn cobject_f64(value: f64) -> _Dart_CObject {
+    _Dart_CObject {
+        type_: Dart_CObject_Type_Dart_CObject_kDouble,
+        value: _Dart_CObject__bindgen_ty_1 { as_double: value },
+    }
+}
+
+fn cobject_bool(value: bool) -> _Dart_CObject {
+    _Dart_CObject {
+        type_: Dart_CObject_Type_Dart_CObject_kBool,
+        value: _Dart_CObject__bindgen_ty_1 { as_bool: value },
+    }
+}
+
+struct PlayerMsg {
+    position: f64,
+    kind: PlayerMsgKind,
+}
+
+#[allow(clippy::enum_variant_names)]
+enum PlayerMsgKind {
+    DurationChanged(f64),
+    PauseChanged(bool),
+    IdleChanged(bool),
+    VideoEnded,
+}
+
+fn send_msg(port: i64, msg: &PlayerMsg) {
+    let (kind, mut value) = match msg.kind {
+        PlayerMsgKind::DurationChanged(duration) => (1, cobject_f64(duration)),
+        PlayerMsgKind::PauseChanged(play_when_ready) => (2, cobject_bool(play_when_ready)),
+        PlayerMsgKind::IdleChanged(idle) => (3, cobject_bool(idle)),
+        PlayerMsgKind::VideoEnded => (4, cobject_i64(0)),
+    };
+
+    let mut values = [
+        &mut cobject_f64(msg.position) as *mut _Dart_CObject,
+        &mut cobject_i64(kind) as *mut _Dart_CObject,
+        &mut value as *mut _Dart_CObject,
+    ];
+
+    let mut msg = _Dart_CObject {
+        type_: Dart_CObject_Type_Dart_CObject_kArray,
+        value: _Dart_CObject__bindgen_ty_1 {
+            as_array: _Dart_CObject__bindgen_ty_1__bindgen_ty_3 {
+                length: values.len() as isize,
+                values: &mut values as *mut *mut _Dart_CObject,
+            },
+        },
+    };
+
+    unsafe {
+        Dart_PostCObject_DL.unwrap()(port, &mut msg);
+    }
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn load(ctx: *mut mpv_handle, url: *const i8) {
+pub unsafe extern "C" fn get_window_handle(player: *const Player) -> HWND {
+    (*player).hwnd
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn load(player: *const Player, url: *const i8, start_position: f64) {
+    (*player)
+        .start_position
+        .store(start_position.to_bits(), atomic::Ordering::SeqCst);
+
     mpv_command(
-        ctx,
+        (*player).ctx,
         &mut [s!("loadfile"), url, std::ptr::null()] as *mut *const i8,
     );
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn pause(ctx: *mut mpv_handle) {
+pub unsafe extern "C" fn pause(player: *const Player) {
     mpv_set_property(
-        ctx,
+        (*player).ctx,
         s!("pause"),
         mpv_format_MPV_FORMAT_FLAG,
         &mut 1 as *mut _ as *mut _,
@@ -117,11 +259,32 @@ pub unsafe extern "C" fn pause(ctx: *mut mpv_handle) {
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn play(ctx: *mut mpv_handle) {
+pub unsafe extern "C" fn play(player: *const Player) {
     mpv_set_property(
-        ctx,
+        (*player).ctx,
         s!("pause"),
         mpv_format_MPV_FORMAT_FLAG,
-        &mut 0 as *mut _ as *mut _,
+        &mut 0 as *mut i32 as *mut c_void,
     );
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn seek_to(player: *const Player, position: f64) {
+    let position = format!("{}\0", position as i64);
+    mpv_command(
+        (*player).ctx,
+        &mut [
+            s!("seek"),
+            position.as_ptr() as *const i8,
+            s!("absolute"),
+            std::ptr::null(),
+        ] as *mut *const i8,
+    );
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn destroy_player(player: *const Player) {
+    let player = Arc::from_raw(player);
+    player.quit.store(true, atomic::Ordering::SeqCst);
+    mpv_wakeup(player.ctx);
 }
