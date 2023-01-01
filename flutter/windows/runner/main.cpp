b26e4deed83673b461bf303311ccc5ac8c7cd55d
Hasan Ali <hasan@hasali.dev>
2023-01-01 23:16:09 +0000

    [flutter] Update window title

--- flutter/windows/runner/main.cpp
+++ flutter/windows/runner/main.cpp
@@ -29,7 +29,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.CreateAndShow(L"zenith_flutter", origin, size)) {
+  if (!window.CreateAndShow(L"Zenith", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
