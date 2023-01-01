d396702f3790717cdccb19e0a58f2bbbf0d7e91f
Hasan Ali <hasan@hasali.dev>
2023-01-01 13:32:57 +0000

    [flutter] Integrate with windows system media controls

--- flutter/video_player/video_player_ffi/src/lib.rs
+++ flutter/video_player/video_player_ffi/src/lib.rs
@@ -2,21 +2,30 @@
 
 mod window;
 
-use std::ffi::{c_void, CStr};
+use std::ffi::{c_char, c_void, CStr};
 use std::sync::atomic::{self, AtomicBool, AtomicU64};
 use std::sync::Arc;
 
 use mpv::{
     mpv_command, mpv_command_async, mpv_create, mpv_event_id_MPV_EVENT_END_FILE,
-    mpv_event_id_MPV_EVENT_LOG_MESSAGE, mpv_event_id_MPV_EVENT_PLAYBACK_RESTART,
-    mpv_event_id_MPV_EVENT_PROPERTY_CHANGE, mpv_event_id_MPV_EVENT_SHUTDOWN,
-    mpv_event_id_MPV_EVENT_START_FILE, mpv_event_log_message, mpv_event_property,
-    mpv_format_MPV_FORMAT_DOUBLE, mpv_format_MPV_FORMAT_FLAG, mpv_format_MPV_FORMAT_INT64,
-    mpv_get_property, mpv_get_property_string, mpv_handle, mpv_initialize, mpv_observe_property,
-    mpv_request_log_messages, mpv_set_option, mpv_set_property, mpv_set_property_string,
-    mpv_terminate_destroy, mpv_wait_event, mpv_wakeup,
+    mpv_event_id_MPV_EVENT_GET_PROPERTY_REPLY, mpv_event_id_MPV_EVENT_LOG_MESSAGE,
+    mpv_event_id_MPV_EVENT_PLAYBACK_RESTART, mpv_event_id_MPV_EVENT_PROPERTY_CHANGE,
+    mpv_event_id_MPV_EVENT_SHUTDOWN, mpv_event_id_MPV_EVENT_START_FILE, mpv_event_log_message,
+    mpv_event_property, mpv_format_MPV_FORMAT_DOUBLE, mpv_format_MPV_FORMAT_FLAG,
+    mpv_format_MPV_FORMAT_INT64, mpv_format_MPV_FORMAT_STRING, mpv_get_property,
+    mpv_get_property_async, mpv_get_property_string, mpv_handle, mpv_initialize,
+    mpv_observe_property, mpv_request_log_messages, mpv_set_option, mpv_set_property,
+    mpv_set_property_string, mpv_terminate_destroy, mpv_wait_event, mpv_wakeup,
+};
+use windows::core::HSTRING;
+use windows::h;
+use windows::Foundation::TypedEventHandler;
+use windows::Media::{
+    MediaPlaybackStatus, MediaPlaybackType, SystemMediaTransportControls,
+    SystemMediaTransportControlsButton, SystemMediaTransportControlsButtonPressedEventArgs,
 };
 use windows::Win32::Foundation::HWND;
+use windows::Win32::System::WinRT::{ISystemMediaTransportControlsInterop, RoGetActivationFactory};
 
 use dart_sdk::{
     Dart_CObject_Type_Dart_CObject_kArray, Dart_CObject_Type_Dart_CObject_kBool,
@@ -31,11 +40,21 @@ macro_rules! s {
     }};
 }
 
+struct SystemMediaControlsButtonHandler {
+    token: windows::Foundation::EventRegistrationToken,
+    handler: *const TypedEventHandler<
+        SystemMediaTransportControls,
+        SystemMediaTransportControlsButtonPressedEventArgs,
+    >,
+}
+
 pub struct Player {
     hwnd: HWND,
     ctx: *mut mpv_handle,
     start_position: AtomicU64,
     quit: AtomicBool,
+    system_media_controls: SystemMediaTransportControls,
+    system_media_controls_button_handler: SystemMediaControlsButtonHandler,
 }
 
 unsafe impl Send for Player {}
@@ -70,11 +89,66 @@ pub unsafe extern "C" fn create_player(
     mpv_observe_property(ctx, 0, s!("pause"), mpv_format_MPV_FORMAT_FLAG);
     mpv_observe_property(ctx, 0, s!("core-idle"), mpv_format_MPV_FORMAT_FLAG);
 
+    const WINDOWS_MEDIA_SYSTEMMEDIATRANSPORTCONTROLS: &windows::core::HSTRING =
+        h!("Windows.Media.SystemMediaTransportControls");
+
+    let interop: ISystemMediaTransportControlsInterop =
+        RoGetActivationFactory(WINDOWS_MEDIA_SYSTEMMEDIATRANSPORTCONTROLS).unwrap();
+
+    let system_media_controls: SystemMediaTransportControls = interop.GetForWindow(hwnd).unwrap();
+
+    system_media_controls.SetIsEnabled(true).unwrap();
+    system_media_controls.SetIsPauseEnabled(true).unwrap();
+    system_media_controls.SetIsPlayEnabled(true).unwrap();
+    system_media_controls.SetIsNextEnabled(true).unwrap();
+    system_media_controls.SetIsPreviousEnabled(true).unwrap();
+
+    let media_display_updater = system_media_controls.DisplayUpdater().unwrap();
+
+    media_display_updater
+        .SetType(MediaPlaybackType::Video)
+        .unwrap();
+
+    media_display_updater.Update().unwrap();
+
+    let button_pressed_handler = Box::leak(Box::new(TypedEventHandler::new({
+        let ctx = ctx as usize;
+        move |_, event: &Option<SystemMediaTransportControlsButtonPressedEventArgs>| {
+            let ctx = ctx as *mut mpv_handle;
+            let event = event.as_ref().unwrap();
+            let button = event.Button().unwrap();
+
+            let pause = match button {
+                SystemMediaTransportControlsButton::Play => false,
+                SystemMediaTransportControlsButton::Pause => true,
+                _ => return Ok(()),
+            };
+
+            mpv_set_property(
+                ctx,
+                s!("pause"),
+                mpv_format_MPV_FORMAT_FLAG,
+                &mut (pause as i32) as *mut i32 as *mut c_void,
+            );
+
+            Ok(())
+        }
+    })));
+
+    let button_handler = SystemMediaControlsButtonHandler {
+        token: system_media_controls
+            .ButtonPressed(button_pressed_handler)
+            .unwrap(),
+        handler: button_pressed_handler,
+    };
+
     let player = Arc::new(Player {
         ctx,
         hwnd,
         start_position: AtomicU64::new(0),
         quit: AtomicBool::new(false),
+        system_media_controls: system_media_controls.clone(),
+        system_media_controls_button_handler: button_handler,
     });
 
     std::thread::spawn({
@@ -112,6 +186,11 @@ unsafe fn run_player_event_loop(player: Arc<Player>, native_port: i64) {
                 is_start = true;
             }
             mpv_event_id_MPV_EVENT_END_FILE => {
+                (*player)
+                    .system_media_controls
+                    .SetPlaybackStatus(MediaPlaybackStatus::Stopped)
+                    .unwrap();
+
                 send_msg(
                     native_port,
                     &PlayerMsg {
@@ -123,10 +202,18 @@ unsafe fn run_player_event_loop(player: Arc<Player>, native_port: i64) {
             mpv_event_id_MPV_EVENT_PLAYBACK_RESTART => {
                 if is_start {
                     is_start = false;
+
+                    (*player)
+                        .system_media_controls
+                        .SetPlaybackStatus(MediaPlaybackStatus::Playing)
+                        .unwrap();
+
                     seek_to(
                         player.as_ref() as *const Player,
                         f64::from_bits(player.start_position.swap(0, atomic::Ordering::SeqCst)),
                     );
+
+                    mpv_get_property_async(ctx, 0, s!("media-title"), mpv_format_MPV_FORMAT_STRING);
                 }
             }
             mpv_event_id_MPV_EVENT_PROPERTY_CHANGE => {
@@ -146,6 +233,15 @@ unsafe fn run_player_event_loop(player: Arc<Player>, native_port: i64) {
                 } else if name.to_bytes() == b"pause" {
                     let value = unsafe { ((*data).data as *mut bool).as_ref() };
                     if let Some(paused) = value {
+                        (*player)
+                            .system_media_controls
+                            .SetPlaybackStatus(if *paused {
+                                MediaPlaybackStatus::Paused
+                            } else {
+                                MediaPlaybackStatus::Playing
+                            })
+                            .unwrap();
+
                         send_msg(
                             native_port,
                             &PlayerMsg {
@@ -167,6 +263,29 @@ unsafe fn run_player_event_loop(player: Arc<Player>, native_port: i64) {
                     }
                 }
             }
+            mpv_event_id_MPV_EVENT_GET_PROPERTY_REPLY => {
+                let data = unsafe { (*event).data as *mut mpv_event_property };
+                let name = unsafe { CStr::from_ptr((*data).name) };
+                if name.to_bytes() == b"media-title" {
+                    if (*data).data.is_null() {
+                        continue;
+                    }
+
+                    let value = unsafe { CStr::from_ptr(*((*data).data as *const *const c_char)) };
+                    let title = value.to_str().unwrap();
+
+                    let media_display_updater =
+                        player.system_media_controls.DisplayUpdater().unwrap();
+
+                    media_display_updater
+                        .VideoProperties()
+                        .unwrap()
+                        .SetTitle(&HSTRING::from(title))
+                        .unwrap();
+
+                    media_display_updater.Update().unwrap();
+                }
+            }
             mpv_event_id_MPV_EVENT_LOG_MESSAGE => {
                 let data = unsafe { (*event).data as *mut mpv_event_log_message };
                 print!("{}", CStr::from_ptr((*data).text).to_str().unwrap());
@@ -174,8 +293,26 @@ unsafe fn run_player_event_loop(player: Arc<Player>, native_port: i64) {
             _ => {}
         }
     }
+
+    player
+        .system_media_controls
+        .RemoveButtonPressed(player.system_media_controls_button_handler.token)
+        .unwrap();
+
+    player.system_media_controls.SetIsEnabled(false).unwrap();
+
+    drop(Box::from_raw(
+        player.system_media_controls_button_handler.handler
+            as *mut TypedEventHandler<
+                SystemMediaTransportControls,
+                SystemMediaTransportControlsButtonPressedEventArgs,
+            >,
+    ));
+
     mpv_terminate_destroy(player.ctx);
     window::close(player.hwnd);
+
+    debug_assert_eq!(Arc::strong_count(&player), 1, "player has a memory leak");
 }
 
 fn cobject_i64(value: i64) -> _Dart_CObject {
