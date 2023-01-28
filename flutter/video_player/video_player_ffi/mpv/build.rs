a20bf49c62a4037fd7e8d1ad653d663014b57682
Hasan Mohsin <hasan@hasali.dev>
2023-01-28 17:25:44 +0000

    [flutter] Update mpv version

--- flutter/video_player/video_player_ffi/mpv/build.rs
+++ flutter/video_player/video_player_ffi/mpv/build.rs
@@ -4,11 +4,12 @@ use std::path::PathBuf;
 fn main() {
     let mpv_dir = PathBuf::from(env::var("MPV_DIR").expect("MPV_DIR must be set"));
 
+    println!("cargo:rerun-if-env-changed=MPV_DIR");
     println!("cargo:rustc-link-search={}", mpv_dir.display());
     println!("cargo:rustc-link-lib=dylib=mpv");
 
     bindgen::builder()
-        .header(mpv_dir.join("include/client.h").to_str().unwrap())
+        .header(mpv_dir.join("include/mpv/client.h").to_str().unwrap())
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .generate()
         .unwrap()
