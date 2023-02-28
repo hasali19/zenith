e02eb2fcaa5543426cdb46fe5d6caabf192f68a3
Hasan Mohsin <hasan@hasali.dev>
2023-02-28 20:52:16 +0000

    [flutter] Set windows sdk include paths

--- flutter/windows/runner/build.rs
+++ flutter/windows/runner/build.rs
@@ -1,6 +1,25 @@
+use std::io;
+use std::path::PathBuf;
+
 use embed_manifest::embed_manifest_file;
 
 fn main() {
-    windres::Build::new().compile("Runner.rc").unwrap();
+    let sdk = get_winsdk_include().unwrap();
+
+    windres::Build::new()
+        .include(sdk.join("um"))
+        .include(sdk.join("shared"))
+        .compile("Runner.rc")
+        .unwrap();
+
     embed_manifest_file("runner.exe.manifest").unwrap();
+}
+
+fn get_winsdk_include() -> io::Result<PathBuf> {
+    let mut paths = std::fs::read_dir(r"C:\Program Files (x86)\Windows Kits\10\Include")?
+        .flatten()
+        .map(|e| e.path())
+        .collect::<Vec<_>>();
+    paths.sort();
+    Ok(paths.into_iter().next_back().unwrap())
 }
