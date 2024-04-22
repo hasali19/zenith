dde0b18472abec7fcbbc676aba4a8eaf6754b2c9
Hasan Mohsin <hasan@hasali.dev>
2024-04-22 22:22:37 +0100

    [flutter] Detailed video info view

--- flutter/lib/routes/item_details/item_details_page.dart
+++ flutter/lib/routes/item_details/item_details_page.dart
@@ -4,9 +4,12 @@ import 'package:auto_route/auto_route.dart';
 import 'package:cast_framework/cast_framework.dart' show CastFrameworkPlatform;
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gap/gap.dart';
 import 'package:sized_context/sized_context.dart';
 import 'package:sliver_tools/sliver_tools.dart';
+import 'package:zenith/api.dart';
 import 'package:zenith/fade_in_image.dart';
+import 'package:zenith/language_codes.dart';
 import 'package:zenith/media_route_button/media_route_button.dart';
 import 'package:zenith/responsive.dart';
 import 'package:zenith/router.dart';
@@ -16,6 +19,7 @@ import 'package:zenith/routes/item_details/widgets/delete_confirmation_dialog.da
 import 'package:zenith/routes/item_details/widgets/episodes_list.dart';
 import 'package:zenith/routes/item_details/widgets/fix_episode_match_dialog.dart';
 import 'package:zenith/routes/item_details/widgets/header.dart';
+import 'package:zenith/theme.dart';
 
 import 'widgets/cast_list.dart';
 
@@ -133,6 +137,123 @@ class _ItemDetailsContentState extends ConsumerState<_ItemDetailsContent> {
                       ),
                       if (widget.state.item.cast.isNotEmpty)
                         CastList(cast: widget.state.item.cast),
+                      SliverToBoxAdapter(
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: isDesktop ? 128 : 16,
+                            vertical: 32,
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Video Info',
+                                style: context.zenithTheme.titleMedium,
+                              ),
+                              const Gap(8),
+                              DefaultTextStyle(
+                                style: context.zenithTheme.bodyMedium,
+                                child: Table(
+                                  defaultColumnWidth:
+                                      const IntrinsicColumnWidth(),
+                                  columnWidths: {
+                                    1: const IntrinsicColumnWidth(),
+                                    2: isDesktop
+                                        ? const IntrinsicColumnWidth()
+                                        : const FlexColumnWidth(),
+                                  },
+                                  children: [
+                                    TableRow(
+                                      children: [
+                                        Text('Format'),
+                                        const SizedBox(width: 16),
+                                        Text(widget
+                                            .state.item.videoFile!.format),
+                                      ],
+                                    ),
+                                    TableRow(
+                                      children: [
+                                        Text('Path'),
+                                        const SizedBox(width: 16),
+                                        Text(widget.state.item.videoFile!.path),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              for (final stream
+                                  in widget.state.item.videoFile!.streams) ...[
+                                const Gap(8),
+                                Text(
+                                  'Stream #${stream.index}',
+                                  style: const TextStyle(
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                DefaultTextStyle(
+                                  style: context.zenithTheme.bodyMedium,
+                                  child: Table(
+                                    defaultColumnWidth:
+                                        const IntrinsicColumnWidth(),
+                                    columnWidths: {
+                                      1: const IntrinsicColumnWidth(),
+                                      2: isDesktop
+                                          ? const IntrinsicColumnWidth()
+                                          : const FlexColumnWidth(),
+                                    },
+                                    children: [
+                                      TableRow(
+                                        children: [
+                                          const Text('Type'),
+                                          const SizedBox(width: 16),
+                                          Text(switch (stream) {
+                                            AudioStreamInfo() => 'Audio',
+                                            VideoStreamInfo() => 'Video',
+                                            _ => 'Unknown',
+                                          }),
+                                        ],
+                                      ),
+                                      TableRow(
+                                        children: [
+                                          const Text('Codec'),
+                                          const SizedBox(width: 16),
+                                          Text(stream.codec),
+                                        ],
+                                      ),
+                                      if (stream is AudioStreamInfo)
+                                        TableRow(
+                                          children: [
+                                            const Text('Language'),
+                                            const SizedBox(width: 16),
+                                            Text(tryResolveLanguageCode(
+                                                stream.language ?? 'Unkown')),
+                                          ],
+                                        ),
+                                      if (stream is VideoStreamInfo)
+                                        TableRow(
+                                          children: [
+                                            Text('Size'),
+                                            const SizedBox(width: 16),
+                                            Text(
+                                                '${stream.width}x${stream.height}'),
+                                          ],
+                                        ),
+                                      if (stream is VideoStreamInfo)
+                                        TableRow(
+                                          children: [
+                                            Text('Crop Size'),
+                                            const SizedBox(width: 16),
+                                            Text(
+                                                '(x1: ${stream.cropX1}, y1: ${stream.cropY1}) (x2: ${stream.cropX2}, y2: ${stream.cropY2})'),
+                                          ],
+                                        ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ],
+                          ),
+                        ),
+                      ),
                       if (widget.state.seasons.isNotEmpty)
                         EpisodesList(
                           groups: widget.state.seasons,
