08adcd57b7595ed3e345fb1e483e1fcf36412169
Hasan Mohsin <hasan@hasali.dev>
2025-01-18 21:56:55 +0000

    [flutter] Update cast notification options

--- flutter/packages/cast_framework/android/src/main/kotlin/dev/hasali/zenith/cast_framework/CastOptionsProvider.kt
+++ flutter/packages/cast_framework/android/src/main/kotlin/dev/hasali/zenith/cast_framework/CastOptionsProvider.kt
@@ -1,15 +1,34 @@
 package dev.hasali.zenith.cast_framework
 
 import android.content.Context
-import com.google.android.gms.cast.CastMediaControlIntent
 import com.google.android.gms.cast.framework.CastOptions
 import com.google.android.gms.cast.framework.OptionsProvider
 import com.google.android.gms.cast.framework.SessionProvider
+import com.google.android.gms.cast.framework.media.CastMediaOptions
+import com.google.android.gms.cast.framework.media.MediaIntentReceiver
+import com.google.android.gms.cast.framework.media.NotificationOptions
 
 @Suppress("unused")
 class CastOptionsProvider : OptionsProvider {
     override fun getCastOptions(context: Context): CastOptions {
+        val notificationOptions = NotificationOptions.Builder()
+            .setActions(
+                mutableListOf(
+                    MediaIntentReceiver.ACTION_SKIP_PREV,
+                    MediaIntentReceiver.ACTION_TOGGLE_PLAYBACK,
+                    MediaIntentReceiver.ACTION_SKIP_NEXT,
+                    MediaIntentReceiver.ACTION_STOP_CASTING,
+                ),
+                intArrayOf(0, 1, 2),
+            )
+            .build()
+
+        val castMediaOptions = CastMediaOptions.Builder()
+            .setNotificationOptions(notificationOptions)
+            .build()
+
         return CastOptions.Builder()
+            .setCastMediaOptions(castMediaOptions)
             .build()
     }
 
