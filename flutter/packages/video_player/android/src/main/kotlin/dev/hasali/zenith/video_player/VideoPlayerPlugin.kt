3a26af957971d4192ba0e9027abf3a262296e080
Hasan Mohsin <hasan@hasali.dev>
2025-09-06 21:28:45 +0100

    Show toast when player error occurs

--- flutter/packages/video_player/android/src/main/kotlin/dev/hasali/zenith/video_player/VideoPlayerPlugin.kt
+++ flutter/packages/video_player/android/src/main/kotlin/dev/hasali/zenith/video_player/VideoPlayerPlugin.kt
@@ -9,6 +9,7 @@ import androidx.media3.common.C
 import androidx.media3.common.MediaItem
 import androidx.media3.common.MediaItem.SubtitleConfiguration
 import androidx.media3.common.MediaMetadata
+import androidx.media3.common.PlaybackException
 import androidx.media3.common.PlaybackParameters
 import androidx.media3.common.Player
 import androidx.media3.common.Player.STATE_IDLE
@@ -496,6 +497,11 @@ private class PlayerInstance(
 
             override fun onTracksChanged(tracks: Tracks) {
                 onEvent?.invoke(Event.TracksChanged(tracks.groups))
+            }
+
+            override fun onPlayerError(error: PlaybackException) {
+                Toast.makeText(context, "Unable to play video", Toast.LENGTH_SHORT)
+                    .show()
             }
         })
 
