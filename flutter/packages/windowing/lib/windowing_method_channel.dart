f6a62179e460c801128e426fab03323a836d092d
Hasan Mohsin <hasan@hasali.dev>
2026-02-15 18:09:15 +0000

    Handle errors from windowing method channel

--- flutter/packages/windowing/lib/windowing_method_channel.dart
+++ flutter/packages/windowing/lib/windowing_method_channel.dart
@@ -7,7 +7,14 @@ const _channel = MethodChannel('zenith.hasali.uk/windowing');
 class MethodChannelWindowing extends WindowingPlatform {
   @override
   Future<WindowController> createController() async {
-    final bool isWindowed = await _channel.invokeMethod('isWindowed');
+    bool isWindowed = true;
+    try {
+      isWindowed = await _channel.invokeMethod('isWindowed');
+    } catch (e) {
+      print(
+        'Failed to determine if application is windowed, defaulting to true',
+      );
+    }
     return MethodChannelWindowController(isWindowed: isWindowed);
   }
 }
@@ -25,7 +32,11 @@ class MethodChannelWindowController extends WindowController {
 
   @override
   Future<void> setFullscreen(bool value) async {
-    await _channel.invokeMethod('setFullscreen', value);
-    _isFullscreen = value;
+    try {
+      await _channel.invokeMethod('setFullscreen', value);
+      _isFullscreen = value;
+    } catch (e) {
+      print('Failed to set fullscreen mode: $e');
+    }
   }
 }
